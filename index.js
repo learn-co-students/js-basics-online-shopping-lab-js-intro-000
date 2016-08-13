4d071f6c2ce9dc05421927c5c788c50b6a99edd4
Peter Tascio <peter.tascio@gmail.com>
2016-08-13 11:09:24 +0000

    Done.

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function getCart(){
 }
 
 function addToCart(item){
-  var item = item;
   var price = Math.floor(Math.random()*100);
-  var list = {item: price};
-  cart.push(item);
+  var list = {};
+  list[item]=price;
+  cart.push(list);
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
@@ -40,4 +40,34 @@ function viewCart(){
     }
   }
   console.log(sent);
+}
+
+function removeFromCart(item){
+  for(var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item)){
+      cart.splice(i, 1);
+      { break };
+    }
+  }
+  console.log("That item is not in your cart.")
+}
+
+function total(){
+  var total = 0;
+  for(var i = 0; i < cart.length; i++){
+    for(var item in cart[i]){
+      total+=cart[i][item];
+    }
+  }
+  return total;
+}
+
+function placeOrder(creditCard){
+  if (creditCard === undefined){
+    console.log('We don\'t have a credit card on file for you to place your order.');
+  }else {
+    var thisTotal = total();
+    console.log(`Your total cost is $${thisTotal}, which will be charged to the card ${creditCard}.`);
+  }
+  cart = [];
 }
--- test/index-test.js
+++ test/index-test.js
@@ -41,7 +41,7 @@ describe('#viewCart', function() {
 
     viewCart();
 
-    
+
 
     expect(console.log).toHaveBeenCalledWith(
       `In your cart, you have socks at $${socksCost}, puppy at $${puppyCost}, iPhone at $${iPhoneCost}.`
@@ -55,78 +55,78 @@ describe('#viewCart', function() {
   });
 });
 
-// describe('#total', function() {
-//   it('adds up the prices of the items in the cart', function() {
-//     addToCart("socks");
-//     addToCart("puppy");
-//     addToCart("iPhone");
-//
-//     const socksCost = getCart()[0]["socks"];
-//     const puppyCost = getCart()[1]["puppy"];
-//     const iPhoneCost = getCart()[2]["iPhone"];
-//
-//     const totalCost = socksCost + puppyCost + iPhoneCost;
-//
-//     expect(total()).toEqual(totalCost)
-//   })
-// })
-//
-// describe('#removeFromCart', function() {
-//   it("removes the item from the cart", function() {
-//     addToCart('pizza')
-//
-//     expect(hasItem(getCart(), 'pizza')).toBe(true)
-//
-//     removeFromCart("pizza");
-//
-//     expect(getCart()).toEqual([]);
-//   });
-//
-//   it("alerts you if you're trying to remove an item that isn't in your cart", function() {
-//     removeFromCart("sock")
-//
-//     expect(console.log).toHaveBeenCalledWith("That item is not in your cart.")
-//   });
-// });
-//
-// describe('#placeOrder', function() {
-//   it("doesn't let you place an order if you don't provide a credit card number", function() {
-//     placeOrder();
-//
-//     expect(console.log).toHaveBeenCalledWith(
-//       "We don't have a credit card on file for you to place your order."
-//     )
-//   });
-//
-//   it("lets you place an order with a credit card", function() {
-//     addToCart('pizza')
-//
-//     const t = total()
-//
-//     placeOrder(123);
-//
-//     expect(console.log).toHaveBeenCalledWith(
-//       `Your total cost is $${t}, which will be charged to the card 123.`
-//     )
-//   });
-//
-//   it('empties the cart', function() {
-//     addToCart('pizza')
-//
-//     expect(hasItem(getCart(), 'pizza')).toBe(true)
-//
-//     placeOrder(123);
-//
-//     expect(getCart()).toEqual([])
-//   })
-// })
-//
-// function hasItem(c, item) {
-//   for (let i = 0, l = c.length; i < l; i++) {
-//     if (c[i].hasOwnProperty(item)) {
-//       return true
-//     }
-//   }
-//
-//   return false
-// }
+describe('#total', function() {
+  it('adds up the prices of the items in the cart', function() {
+    addToCart("socks");
+    addToCart("puppy");
+    addToCart("iPhone");
+
+    const socksCost = getCart()[0]["socks"];
+    const puppyCost = getCart()[1]["puppy"];
+    const iPhoneCost = getCart()[2]["iPhone"];
+
+    const totalCost = socksCost + puppyCost + iPhoneCost;
+
+    expect(total()).toEqual(totalCost)
+  })
+})
+
+describe('#removeFromCart', function() {
+  it("removes the item from the cart", function() {
+    addToCart('pizza')
+
+    expect(hasItem(getCart(), 'pizza')).toBe(true)
+
+    removeFromCart("pizza");
+
+    expect(getCart()).toEqual([]);
+  });
+
+  it("alerts you if you're trying to remove an item that isn't in your cart", function() {
+    removeFromCart("sock")
+
+    expect(console.log).toHaveBeenCalledWith("That item is not in your cart.")
+  });
+});
+
+describe('#placeOrder', function() {
+  it("doesn't let you place an order if you don't provide a credit card number", function() {
+    placeOrder();
+
+    expect(console.log).toHaveBeenCalledWith(
+      "We don't have a credit card on file for you to place your order."
+    )
+  });
+
+  it("lets you place an order with a credit card", function() {
+    addToCart('pizza')
+
+    const t = total()
+
+    placeOrder(123);
+
+    expect(console.log).toHaveBeenCalledWith(
+      `Your total cost is $${t}, which will be charged to the card 123.`
+    )
+  });
+
+  it('empties the cart', function() {
+    addToCart('pizza')
+
+    expect(hasItem(getCart(), 'pizza')).toBe(true)
+
+    placeOrder(123);
+
+    expect(getCart()).toEqual([])
+  })
+})
+
+function hasItem(c, item) {
+  for (let i = 0, l = c.length; i < l; i++) {
+    if (c[i].hasOwnProperty(item)) {
+      return true
+    }
+  }
+
+  return false
+}
