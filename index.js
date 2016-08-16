106debabfe4bc7f32e130070f6d77a8dfd091bb1
maxgrok <maxgrok2011@gmail.com>
2016-08-16 14:17:15 -0400

    Done.

--- index.js
+++ index.js
@@ -24,17 +24,54 @@ function addToCart(item){
 	//adding item & price to cart as object
 	cart.push({item: price});
 	//item has been added
-	console.log(item +" has been added to your cart.")
+	console.log(item +" has been added to your cart.");
 	//return cart
 	return cart;
+	
 }
 function viewCart(){
-	for(var i = 0; i <= cart.length; i++){
+	//define item variablevar mealNames = Object.keys(meals)
+ /* var mealNames = Object.keys(meals)
+
+for (var i = 0, l = mealNames.length; i < l; i++) {
+  console.log(`I ate ${meals[mealNames[i]]} for ${mealNames[i]}!`)
+}
+
+for (var mealName in meals) {
+  console.log(`I ate ${meals[mealName]} for ${mealName}!`)
+}
+*/
+	var items = Object.keys(cart);
 		if(cart.length === 0){
 			console.log("Your shopping cart is empty.");
 		}
 		else{
-			console.log("In your cart you have "+ cart.item + "at" + cart.price + "."); 
+			for(var items in cart){
+			for(var i =0;i< cart.length;i++){
+			console.log("In your cart you have "+ cart[i] + "at" + items + ".");
+			}	
 		}
 	}
 }
+function removeFromCart(item){
+	//if item not in cart, then print out 'That item is not in your cart'
+
+	//if item in cart, remove item from cart array
+	
+	//then return cart
+	return card;
+	//HINT: Check each object's key to see if it matches the parameter, 
+	//then remove it if it matches. You might find hasOwnProperty to be useful.) 
+}
+
+function placeOrder(cardNumber){
+	if (cardNumber === null || cardNumber === undefined){
+		console.log("We don\'t have your credit card on file for you to place your order.")
+	}
+	else{
+		console.log('Your total cost is $${total()}, which will be charged on the card ${cardNumber}.')
+	}
+	//if no arg received, print out 'We don't have your credit card on file for you to place your order.'
+	//if cc on file, then print out "Your total cost is $${total()}, which will be charged to the card ${cardNumber}."
+}
+
