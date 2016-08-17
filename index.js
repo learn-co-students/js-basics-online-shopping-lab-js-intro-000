c7216bdbebc5095083614e167cb6264ff514ebac
maxgrok <maxgrok2011@gmail.com>
2016-08-17 17:06:05 -0400

    Done.

--- index.js
+++ index.js
@@ -41,26 +41,40 @@ function viewCart(){
 				var key = Object.keys(cart[i])[0];
 				array.push(`${key} at \$${cart[i][key]}`);
 			}
-			console.log(`In your cart you have ${array.join(", ")}.`);
+			console.log(`In your cart, you have ${array.join(", ")}.`);
 		}
 }
 function removeFromCart(item){
 	//if item not in cart, then print out 'That item is not in your cart'
-	/*if (item != ){
-		console.log("That item is not in your cart");
+	//get all items in cart
+
+	var cart ={
+		pizza: '63', //don't forget commas
+	//adding more key-value pairs
+		calamari: '75',
+		mushrooms: '10',
+		steak: '45'
 	}
-	//if item in cart, remove item from cart array
-	//for ( in cart){
-		//remove item from cart array
-		cart.slice(item);
-		//then return cart
-		return cart;
-	}
-	//HINT: Check each object's key to see if it matches the parameter, 
-	//then remove it if it matches. You might find hasOwnProperty to be useful.)*/ 
+	var items = Object.keys(cart); 
+		//if cart does not have item in it:
+			if (cart.hasOwnProperty(item) === false){
+				console.log(`That is not in your cart.`);
+			}
+			//if cart has item in it
+		 	else if (cart.hasOwnProperty(item) === true){
+				// console.log(`That ${item} is in your cart. It costs $${cart[item]}.`);
+				// Need to find where item is in the object
+				// convert item from string to key? 
+				// find index of the item in items
+				delete cart["item"];
+				console.log(cart); //returned new array to how deleted item
+			}
 }
 
-function placeOrder(cardNumber){
+removeFromCart("pizza");
+removeFromCart('iPhone');
+
+/*function placeOrder(cardNumber){
 	if (cardNumber === null || cardNumber === undefined){
 		console.log("We don\'t have your credit card on file for you to place your order.")
 	}
@@ -70,4 +84,4 @@ function placeOrder(cardNumber){
 	//if no arg received, print out 'We don't have your credit card on file for you to place your order.'
 	//if cc on file, then print out "Your total cost is $${total()}, which will be charged to the card ${cardNumber}."
 }
-
+*/
