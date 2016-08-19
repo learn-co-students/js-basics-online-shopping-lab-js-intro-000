65d8b76139756bcf00efc0c01f6ed5d308c0190c
Rachel Lim <rslim87@gmail.com>
2016-08-19 15:43:04 +0000

    Done.

--- index.js
+++ index.js
@@ -61,12 +61,12 @@ if (!itemInCart){
 return cart
 }
 
-function placeOrder(creditCardNumber) {
-  if (!creditCardNumber) {
+function placeOrder(cardNumber) {
+  if (!cardNumber) {
     return console.log("We don't have a credit card on file for you to place your order.")
   }
 
-  console.log("Your total cost is " +  "$" + total() + " , which will be charged to the card " + creditCardNumber + ".")
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
 
   cart = []
 }
