aed7040a6e8829318c6c88ea6716342b3cfaca33
pwjablonski Jablonski <pwjablonski@gmail.com>
2016-08-19 02:26:32 +0000

    test

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function setCart(newCart) {
   cart = newCart;
 }
 
+function getCart() {
+  return cart;
+}
+
 function total() {
   let t = 0
 
@@ -14,4 +18,25 @@ function total() {
   }
 
   return t
+}
+
+function addToCart(item){
+  cart.push(item)
+  console.log(item + ' has been added to your cart.');
+  return cart
+}
+
+function viewCart(){
+  if (cart.length === 0){
+    return 'Your shopping cart is empty.'
+  }
+  else {
+    var printItems = "'In your cart, you have "
+    for (var i = 0, l = cart.length; i < l; i++) {
+      for (var item in cart[i]) {
+        printItems += item + "at " + "$"
+      }
+    }
+    console.log(printItems)
+  }
 }
