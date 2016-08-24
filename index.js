14891422cf8e405045d67934bbcc0007fd1cd9ee
Ray Mulligan <rmullig2@optonline.net>
2016-08-24 02:28:33 +0000

    Done.

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function removeFromCart(item) {
   else {
     for (let i = 0; i < cart.length; i++) {
       if (cart[i].hasOwnProperty(item)) {
-        delete cart[i];
+        cart.splice(i,1);
         return cart
       }
     }
