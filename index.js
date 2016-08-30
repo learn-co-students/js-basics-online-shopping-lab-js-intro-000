856aef3ed93ec22341441ce2552887df7aee46e8
Shaw Li <shaw.s.li@gmail.com>
2016-08-30 20:11:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 var cart = [];
 
+function setCart(newCart) {
+  cart = newCart;
+}
 
-function addToCart(purchase) {
+
+function addToCart(item) {
   var price = Math.floor(Math.random()*10);
-  var object = {[purchase]: price};
+  var object = {[item]: price};
   cart.push(object);
-  console.log(purchase + ' has been added to your cart.');
+  console.log(item + ' has been added to your cart.');
+  console.log(cart.length);
   return(cart)
 }
 
-function viewCart() {
-  if (cart.length !=0) {
-
-
-    for (var mealName in meals) {
-      console.log(`I ate ${meals[mealName]} for ${mealName}!`)
-    }
-
-    for (var item in cart) {
-      return(`${cart[item]} for ${mealName}`)
-    }
+function viewCart(){
+  if (cart.length === 0){
+    console.log('Your shopping cart is empty.')
   }else {
-    return('Your shopping cart is empty.')
+    var statement = 'In your cart, you have '
+    for (var i = 0, l = cart.length; i < l; i++) {
+      for (var item in cart[i]){
+        statement = (statement + `${item} at $${cart[i][item]}`);
+        if (i+1 === cart.length){
+          statement = (statement + ".");
+          console.log(statement);
+          return(statement)
+        }else
+          statement = (statement + ', ')
+      }
+    }
   }
-}
-
-function setCart(newCart) {
-  cart = newCart;
 }
 
 function total() {
@@ -41,6 +45,44 @@ function total() {
   return t
 }
 
+function removeFromCart(item) {
+  for (var i = 0, l = cart.length; i < l; i++) {
+    if(cart[i].hasOwnProperty(item)){
+        cart.splice(i, 1);
+        return(cart)
+    }
+  }
+  console.log('That item is not in your cart.')
+}
+
+function placeOrder(cardNumber) {
+  if (cardNumber != null){
+    var totalCost = total();
+    console.log(`Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`)
+                
+    cart = [];
+
+  }else{
+    console.log("We don't have a credit card on file for you to place your order.")
+  }
+}
+
 function getCart() {
   return(cart)
 }
+
+
+/*
+
+if(cart ===[]){
+    return('Your shopping cart is empty.')
+}else{
+    return('In your cart, you have')
+}
+}
+
+
+
+
+
+*/
