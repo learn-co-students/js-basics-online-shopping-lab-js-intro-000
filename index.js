f2edf238bdc5d346f670076f773f9b22fb4eec3d
Clinton Rhodes <clint.xyzy@gmail.com>
2016-08-31 14:34:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var cart;
-
+var cart = [];
+console.log('test');
 function setCart(newCart) {
   cart = newCart;
 }
@@ -14,4 +14,30 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  let price = Math.floor(Math.random()*100);
+  let addIt = {[item]:price};
+  cart.push(addIt);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart(){
+    if (cart.length <= 0) { console.log("Your shopping cart is empty."); }
+    else {
+    let newCart = [];
+    for (let i = 0; i < cart.length; i+=1) {
+        let cartKey = Object.keys(cart[i])[0];
+        //console.log(cartKey);
+        newCart.push(cartKey + " at $" + cart[i][cartKey]);
+        }
+    console.log("In your cart, you have " + newCart + ".");
+  //return newCart;
+    }
 }
