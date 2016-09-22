472f093fd96990d50f4e58dcdfb6f7e54165ebf9
Adam Nur <anur@comcast.net>
2016-09-22 05:30:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
-var cart;
+var cart=[]
+function getCart(){
+  return cart
+}
+function addToCart(item){
+  var price=Math.floor((Math.random()*10))
+  var product={[item]:price}
+  cart.push(product)
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+function viewCart(){
+  if (cart.length==0){
+    console.log( "Your shopping cart is empty.")
+  }
+  var string="In your cart, you have"
+  for( var i=0;i<cart.length;i++){
+
+     string+= ` ${Object.keys(getCart()[i])} at $${getCart()[i][Object.keys(getCart()[i])]},`
+  }
+     string=string.substring(0, string.length-1)
+     string+='.'
+  console.log(string)
+}
+function placeOrder(number){
+  if (number ==undefined){
+    console.log( "We don't have a credit card on file for you to place your order.")
+  }
+
+    console.log( `Your total cost is $${total()}, which will be charged to the card ${number}.`)
+
+  cart=[]
+  return cart
+}
 
 function setCart(newCart) {
   cart = newCart;
 }
+
+function removeFromCart(item){
+  for(var i=0;i<cart.length;i++){
+    if(cart[i].hasOwnProperty(item)){
+      cart.splice(i)
+      return cart
+
+    }
+  }
+  console.log("That item is not in your cart.")
+
+  return cart
+}
+
 
 function total() {
   let t = 0
