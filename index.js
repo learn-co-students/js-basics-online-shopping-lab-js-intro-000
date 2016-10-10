f7b0e60e4ce21761e20cc18b6e316070398ae2a2
Panagiotis Skarlas <pskarl@gmail.com>
2016-10-10 16:25:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-var cart;
+var cart = new Array();
 
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  cart[item] = Math.floor(Math.random() * 100);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length > 0) {
+    for (var item in cart) {
+      console.log(`In your cart you have ${item} - ${cart[item]}.`);
+    }
+    } else {
+      console.log("Your shopping cart is empty.");
+    }
+}
+function removeFromCart(item) {
+  cart.hasOwnProperty('item') ? delete cart['item'] : console.log("That item is not in your cart.");
+  return cart;
+}
+function placeHolder(cardNumber) {
+  var creditCardEmpty = cardNumber;
+  typeof creditCardEmpty !== 'undefined' ? console.log("We don't have a credit card on file for you to place your order.") :
+  console.log(`"Your total cost is $${total()}, which will be charged to the card ${cardNumber}."`);
+}
 function setCart(newCart) {
   cart = newCart;
 }
