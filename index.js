a8955442a5b0a9677ce5043aacf34aba32b9fccc
crisam degracia <crisamdegracia@gmail.com>
2016-10-10 16:23:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-var cart ;
-
-
+var cart = {}
 
 function setCart(newCart) {
   cart = newCart;
 }
-
 function total() {
   let t = 0
-  var allPrice = cart.item + cart.socks + cart.puppy + cart.iPhone ;
-
   for (var i = 0, l = cart.length; i < l; i++) {
     for (var item in cart[i]) {
       t += cart[i][item]
     }
   }
-    return t + allPrice
+    return t
 }
+function getCart() { return cart}
 
-function getCart() { return cart
-
-}
 function addToCart(item){
-  var itemPrice =  Math.random()
-  var finalPrice = Math.floor(itemPrice)
-  var itemNPrice = {'item':'finalPrice'}
+  var itemPrice =  Math.floor()
+  var itemNPrice = {item:itemPrice}
   cart.push(itemNPrice)
   console.log (`${item} has been added to your cart.`)
   return cart
@@ -42,4 +34,18 @@ for ( var i = 0 ; i < cart.length ; i++ ){
 console.log (`In your cart, you have socks at $${cart.socks}, puppy at $${cart.puppy}, iPhone at $${cart.iPhone}.`)
 
 } console.log ('Your shopping cart is empty.')
+}
+
+
+function removeFromCart(itemName){
+  var  i = 0
+  var itemName = cart
+if (itemName.hasOwnProperty(cart[i]) === cart[i]){
+itemName.slice(0,1)
+return cart
+i++
+}
+
+else{
+} console.log("That item is not in your cart.")
 }
