f4bec4fbcb67edcbdd0ba0910cf0d361a4e5cbc9
crisam degracia <crisamdegracia@gmail.com>
2016-10-12 14:49:10 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function total() {
 function getCart() { return cart}
 
 function addToCart(item){
-  var itemPrice =  Math.floor(Math.random()*100)
+  var itemPrice =  Math.floor()
   var itemNPrice = {item:itemPrice}
   cart.push(itemNPrice)
   console.log (`${item} has been added to your cart.`)
@@ -23,9 +23,9 @@ function addToCart(item){
 }
 
 function viewCart(){
-  var s = {socks: 'Math.random' }
-  var p = {puppy :'Math.random'}
-  var i = {iPhone :'Math.random'}
+  var s = {socks:Math.floor()}
+  var p = {puppy:Math.floor()}
+  var i = {iPhone:Math.floor()}
   cart.push(s)
   cart.push(p)
   cart.push(i)
@@ -37,15 +37,31 @@ console.log (`In your cart, you have socks at $${cart.socks}, puppy at $${cart.p
 }
 
 
-function removeFromCart(itemName){
-  var  i = 0
-  var itemName = cart
-if (itemName.hasOwnProperty(cart[i]) === cart[i]){
-itemName.slice(0,1)
-return cart
-i++
+function removeFromCart(v){
+  var toFalse = false
+  for ( var i = 0 ; i < cart.length + 1 ; i++)
+  {
+if (cart[i].hasOwnProperty(v)){
+ toFalse = true
+cart.splice(i,i+1)
+}
+}
+ if (toFalse === false) {
+  console.log("That item is not in your cart.")
+}
 }
 
-else{
-} console.log("That item is not in your cart.")
+function placeOrder(card){
+
+for ( let i=0; i < cart.length ; i++){
+  if(cart.hasOwnProperty(card)){
+    console.log(`Your total cost is ${total()}, which will be charged to the ${card}`)
+  }
+  else if (cart.hasOwnProperty(card))
+  cart.splice(i,i+1)
+  else {
+    console.log (" We don\''t have a credit card on file for you to place your order.")
+  }
+}
+
 }
