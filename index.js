629ea43b3508a7b619cb69643c3bfbbcae90d7a2
Robert Bene <robcbene@hotmail.com>
2016-10-19 17:22:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
-var cart;
+var cart = [];
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var b = Math.random() * 100
+  var c = Math.floor(b)
+  cart.push({[item]: c})
+  console.log (`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length === 0) {
+    console.log (`Your shopping cart is empty.`);
+  }
+  else {
+    var newArr = [];
+  for (var i = 0; i < cart.length; i++) {
+    var itemWithPrice = cart[i];
+    var item = Object.keys(itemWithPrice)[0];
+    var price = itemWithPrice[item];
+    newArr.push(` ${item} at \$${price}`);
+  }
+  console.log(`In your cart, you have${newArr}.`);
+}}
+
+function removeFromCart(item) {
+  for (var i in cart) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
+}
+
+function placeOrder(card) {
+  if (card === undefined) {
+    console.log("We don\'t have a credit card on file for you to place your order.");
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${card}.`);
+    cart = [];
+  }
+}
+
 
 function setCart(newCart) {
   cart = newCart;
