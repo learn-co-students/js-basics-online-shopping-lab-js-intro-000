4f41144c701ca3bc919695e7d8265d6a67acf1cc
Mary Irwin <maldenirwin@gmail.com>
2016-10-21 02:10:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart= [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,27 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+return cart;
+}
+
+ function addToCart(item) {
+   var price= Math.floor(Math.random() * 100);
+   cart.push ({[item]:price});
+   console.log(item + "has been added to your cart.");
+   return cart;
+ }
+
+function viewCart() {
+if (cart.length==0) {
+return "Your shopping cart is empty.";
+}
+else {
+  var count=0
+  while (count<cart.length) {
+    return "You have a" + {[item]:price} + ".";
+    }
+  }
 }
