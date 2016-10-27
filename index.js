aff5be1d568d6011d22c680fe0ee7724760df769
David Booker <david.booker1@gmail.com>
2016-10-27 18:11:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -15,3 +15,50 @@ function total() {
 
   return t
 }
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor((Math.random() * 100) + 1);
+  cart.push({[item]: price});
+  console.log(item + ' has been added to your cart.');//this will not work if interpolate item as ${item}
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length > 0) {
+      for (var item in cart) {
+        console.log('In your cart you have ' + Object.keys(item) + ' at ' + cart[item] + '.');
+      }
+   return cart;
+  }
+  else {
+    console.log('Your shopping cart is empty.')
+    }
+}
+
+function removeFromCart(item) {
+      for (var item in cart) {
+        if (cart.hasOwnProperty(item)) {
+        delete cart[item];
+        return cart;
+        }
+        else {
+          console.log('That item is not in your cart.');
+        }
+      }
+}
+
+/*function placeOrder(cardNumber) {
+  if cardNumber.isNaN() {
+    console.log('We don\'t have a credit card on file for you to place your order.');
+  }
+  else {
+    console.log('Your total cost is $${total()}, which will be charged to the card ${cardNumber}.');
+    cart = [];
+    return cart;
+  }
+}
+*/
