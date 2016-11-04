67ee339e73594532c025b5f9fff49dca1b031f85
Kevin Hwang <kev.r.hwang@gmail.com>
2016-11-04 14:31:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,38 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random(0,1) * 100);
+  cart.push({[item]: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var cartList = [];
+    for (var things in cart) {
+      var price = cart[things];
+      cartList.push(`${things} at $${price}`)
+    }
+    console.log(`In your cart, you have ${cartList.join(', ')}.`)
+  }
+}
+
+function removeFromCart(item) {
+  for (var things in cart) {
+    if (things !== item) {
+      console.log("That item is not in your cart.")
+    } else if (things === item) {
+      cart.splice(things)
+    }
+  } return cart
 }
