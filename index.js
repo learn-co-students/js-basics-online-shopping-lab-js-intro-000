a34beebebdfbfb50bfa9f30b835c7eaff7344c23
Vanessa Jimenez <vjimenez127@gmail.com>
2016-11-05 23:46:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-var cart;
+var cart=[];
 
 function setCart(newCart) {
   cart = newCart;
+}
+
+function getCart(){
+  return cart
+}
+
+function addToCart(item){
+  var price= Math.floor((Math.random()* 100) + 0);
+  cart.push({item: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+var itemPrices = Object.keys(cart)
+
+function viewCart(){
+  if(cart.length===0){
+    console.log("Your shopping cart is empty.");
+  } else{
+    for(var i=0; i<cart.length; i++){
+      console.log(`In your cart you have ${item[i]} for ${item[itemPrices[i]]}.`);
+    }
+  }
+}
+
+function removeFromCart(){
+
 }
 
 function total() {
