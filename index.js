bb69ea62e5090be6b30d596562dd9e0edf9bb323
Robert Hitt <robhitt@gmail.com>
2016-11-09 15:33:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
 }
 
 function total() {
-  let t = 0
+  let tot = 0;
 
   for (var i = 0, l = cart.length; i < l; i++) {
     for (var item in cart[i]) {
-      t += cart[i][item]
+      tot = tot + cart[i][item];
     }
   }
 
-  return t
+  return tot;
+}
+
+function getCart() {
+  return cart
+}
+
+function addToCart(item) {
+  var price = Math.floor((Math.random() * 100) + 1);
+  //cart[item] = price;
+  cart.push({item: price});
+  console.log(`${item} has been added to your cart.`)
+  return cart;
+}
+
+function viewCart() {
+  var cartArr = [];
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    for (var index in getCart()) {
+      var key = Object.keys(cart[index]);
+      var cost = cart[index][key];
+      cartArr.push(` ${key} at ${cost}`);
+
+    }
+    var newCart = cartArr.join(",");
+    console.log(`In your cart, you have${newCart}.`)
+  }
+}
+
+function removeFromCart(item) {
+
 }
