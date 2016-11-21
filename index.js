48eb248a8b7c3ec5de20c4033e656e6b5eca5582
Luis Alves <lmoalves@gmail.com>
2016-11-21 22:09:37 +0000

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,29 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+    return cart;
+}
+
+function addToCart(item) {
+    var price = Math.floor(Math.random() * 100);
+    cart.push({[item] : price});
+    console.log(`${item} has been added to your cart.`);
+    return cart;
+}
+
+function viewCart() {
+    var temp = 'In your cart, you have ';
+    if (cart.length == 0) {
+            console.log("Your shopping cart is empty.");
+    } else {
+        for (var i = 0; i < cart.length; i++) {
+            for (var variable in cart[i]) {
+                temp += `${variable} at $${cart[i][variable]}, `;
+            }
+        }
+    console.log(temp.substring(0, temp.length-2) + ".");
+    }
 }
