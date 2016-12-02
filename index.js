18a32459eff62a714ae9052a2ef0157a3f1f4d0d
Ines Pucker <ines.pucker@gmx.de>
2016-12-02 09:19:51 +0000

    Done.

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ function total() {
 
 var cart = [];
 
-undefined
 function getCart (){
 return cart
 }
-
+ 
 
 function addToCart (item) {
 var price = Math.floor(Math.random()*100);
-cart.push ({[price]:item})
+cart.push ({[item]:price})
 console.log (item + " has been added to your cart.")
 return getCart ()
 }
 
+
 function viewCart (){
-if (cart.length == 0) {
+if (cart.length === 0) {
 console.log ("Your shopping cart is empty.")
 }
 else {
@@ -43,11 +43,11 @@ var text = '';
 for (var i=0; i<(cart.length-1); i++) {
 textobject = cart [i];
 textkeyarray = Object.keys (textobject);
-text = text + textobject[textkeyarray] +" at $" + textkeyarray[0] + ", ";
+text = text + textkeyarray[0] +" at $" + textobject[textkeyarray] + ", ";
 }
 textobject = cart [i++];
 textkeyarray = Object.keys (textobject);
-  console.log ("In your cart, you have " + text + textobject[textkeyarray] +" at $" + textkeyarray[0] + "!")
+  console.log ("In your cart, you have " + text + textkeyarray[0] +" at $" + textobject[textkeyarray[0]] + ".")
 return
 }
 }
@@ -59,12 +59,12 @@ var text = '';
 for (var i=0; i<(cart.length); i++) {
 textobject = cart [i];
 textkeyarray = Object.keys (textobject);
-if (item == textobject[textkeyarray]) {
+if (item === textkeyarray[0]) {
 cart.splice (i,1);
+}
+else console.log ('That item is not in your cart.');
+}
 return cart
-}
-else console.log ("That item is not in your cart.")
-}
 }
 
 
@@ -75,8 +75,9 @@ console.log ("We don't have a credit card on file for you to place your order.")
 else {
 for (var i=0, costs=0; i<(cart.length); i++) {
 textobject = cart [i];
+
 textkeyarray = Object.keys (textobject);
-costs = costs + Number (textkeyarray[0]);
+costs = total ();
 }
 cart.splice (0, cart.length);
 console.log ("Your total cost is $" + costs + ", which will be charged to the card " + creditcardnumber +".")
