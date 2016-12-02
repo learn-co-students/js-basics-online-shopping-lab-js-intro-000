17827aca919f7c7ecce57b94d0cdf7826fce8488
Ines Pucker <ines.pucker@gmx.de>
2016-12-02 10:52:29 +0000

    Done.

--- index.js
+++ index.js
@@ -56,17 +56,18 @@ function removeFromCart (item){
 var textobject = {};
 var textkeyarray = [];
 var text = '';
+if (cart.length>0){
 for (var i=0; i<(cart.length); i++) {
 textobject = cart [i];
 textkeyarray = Object.keys (textobject);
 if (item === textkeyarray[0]) {
 cart.splice (i,1);
-}
-else
-{console.log ('That item is not in your cart.');
-}
-}
 return cart
+}
+else console.log ("That item is not in your cart.")
+}
+}
+else console.log ("That item is not in your cart.")
 }
 
 
@@ -75,7 +76,7 @@ if (typeof creditcardnumber != "number"){
 console.log ("We don't have a credit card on file for you to place your order.")
 }
 else {
-for (var i=0, costs=0, textobject={}; i<(cart.length); i++) {
+for (var i=0, costs=0, textobject={}, textkeyarray =[]; i<(cart.length); i++) {
 textobject = cart [i];
 
 textkeyarray = Object.keys (textobject);
