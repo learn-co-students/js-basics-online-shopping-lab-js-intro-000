899f30726923ad626600b169ad768b837d6479ba
Louis Gaeta <louis.gaeta1@gmail.com>
2016-12-14 23:05:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart=[];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,55 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  var price=Math.floor(Math.random()*10)
+  cart.push({item: price});
+  console.log(item+" has been added to your cart.")
+  return cart;
+}
+
+function viewCart(){
+  var keyNames=[];
+  for(let i=0;i<cart.length;i++){
+    keyNames[i]=Object.keys(cart[i]);
+  }
+  if(cart.length===0){
+    console.log("Your shopping cart is empty.")
+  }
+  else{
+    console.log("In your cart, you have ")
+    for (let i=0;i<cart.length;i++){
+      console.log(`${keyNames[i]} at ${cart[i][keyNames[i]]}`)
+    }
+  }
+}
+
+
+function removeFromCart(item){
+  if(cart.hasOwnProperty(item)){
+    //?????
+    return cart;
+  }
+  else {
+    console.log("That item is not in your cart.")
+    return cart;
+  }
+}
+
+function placeOrder(number){
+  if(!number){
+    console.log("We don't have a credit card on file for you to place your order.")
+  }
+  else{
+    console.log(`Your total cost is $${total()}. which will be charged to the card ${number}`);
+  }
+  for(let i=0;i<cart.length;i++){
+    cart.shift();
+  }
 }
