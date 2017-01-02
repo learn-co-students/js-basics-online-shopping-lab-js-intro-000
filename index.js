297d2e4ffd7b004b5e9eb884f034f490ae21b93d
idith almog <almog.idith@gmail.com>
2017-01-02 02:28:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+
 
 function setCart(newCart) {
   cart = newCart;
@@ -15,3 +15,15 @@ function total() {
 
   return t
 }
+
+function getCart() {return cart}
+var cart=[]
+function addToCart(item) {
+                          cart.item=Math.random()*100;
+                          console.log(`${item} has been added to your cart.`);
+
+                          return cart}
+var item=Object.keys(cart)
+function viewCart() {if (cart.length<1) {console.log (`Your shopping cart is empty.`)}
+                      else
+                        {for (var item in cart) {console.log(`In your cart, you have ${item} and ${cart[item]} pairs.)}}}
