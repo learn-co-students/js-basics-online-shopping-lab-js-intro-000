becd0f05fadc678f7acb45da55981801e9ff0a41
Naz Khan <nazmuskhan@yahoo.com>
2017-01-11 18:33:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = []
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,43 @@ function total() {
   }
 
   return t
+}
+function getCart() {
+  return cart
+}
+function addToCart(item) {
+  var price = Math.floor(Math.random()*100)
+  cart.push({[item] : price})
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+function viewCart() {
+  if (cart.length === 0){
+      console.log("Your shopping cart is empty.")
+    }else{
+      var cartArray = []
+      var output = "In your cart, you have"
+      for (var i = 0 ; i < cart.length ; i++) {
+         var item = Object.keys(cart[i])
+         cartArray.push(` ${item} at $${cart[i][item]}`)
+       }
+    console.log(output + cartArray + ".")
+  }
+}
+function removeFromCart(item) {
+  for (var i = 0; i < cart.length ; i++) {
+    if (cart[i].hasOwnProperty(item) === true) {
+      cart.shift(item)
+      return cart
+    }
+  }
+      console.log("That item is not in your cart.")
+}
+
+function placeOrder(ccN) {
+  if (ccN == null){
+    var text = 'We don\'t have a credit card on file for you to place your order.'
+    console.log(text)
+  }console.log(`Your total cost is $${total()}, which will be charged to the card ${ccN}.`)
+  return cart = []
 }
