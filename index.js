20b9d46449e4e9e7a40725f59e4b7380d762ab75
idith almog <almog.idith@gmail.com>
2017-01-15 02:45:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 
-
+var cart=[]
 function setCart(newCart) {
   cart = newCart;
 }
 
-function total() {
-  let t = 0
 
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item]
-    }
-  }
 
-  return t
-}
+
 
 function getCart() {return cart}
-var cart=[]
+
 function addToCart(item) {
-                          cart.item=Math.random()*100;
+                          // cart.push({item:Math.random()*100});
+                          cart.push({[item]:Math.floor(Math.random()*100)});
                           console.log(`${item} has been added to your cart.`);
 
                           return cart}
-var item=Object.keys(cart)
+
+function total() {
+  let t = 0
+for (var i = 0, l = cart.length; i < l; i++) {
+    var item=Object.keys(cart[i])[0]
+    t += cart[i][item]
+  }
+
+
+return t
+}
 function viewCart() {if (cart.length<1) {console.log (`Your shopping cart is empty.`)}
                       else
-                        {for (var item in cart) {console.log(`In your cart, you have ${item} and ${cart[item]} pairs.)}}}
+                      var conlog='In your cart, you have'
+                       {for (var i = 0, l = cart.length; i < l-1; i++)
+                        conlog=conlog+` ${Object.keys(cart[i])[0]} at ${cart[i][Object.keys(cart[i])[0]]}, `}
+                        conlog=conlog+` ${Object.keys(cart[cart.length-1])[0]} at ${cart[cart.length-1][Object.keys(cart[cart.length-1])[0]]}. `;
+
+
+                        console.log(conlog)}
+
+ function removeFromCart (itemname) {for (var i = 0, l = cart.length; i < l; i++) if (Object.keys(cart[i])[0]==[itemname])
+                                    { cart.splice(i-1,1)}
+
+                                    else {console.log (`That item is not in your cart.`)}
+                                    ;return cart
+                                  }
+function placeOrder(cardNumber) {if (cardNumber===undefined) {console.log(`We don'\t have a credit card on file for you to place your order.`)}
+                                  else {console.log (`Your total cost is $ ${total()}, which will be charged to the card ${cardNumber}.`);
+                                  cart=[]; return cart } }
