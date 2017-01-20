d205f9522e1313f97ca0dbaf02c671e3d4ab7259
Leon Mercanti <digital_domain@hotmail.com>
2017-01-20 02:10:43 +0000

    Done.

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ function total() {
   return t
 };
 
+
 function getCart()  {
   return cart;
 };
+
 
 function addToCart(item)  {
   var price = Math.floor(Math.random(0, 100) * 100);
@@ -29,6 +31,7 @@ function addToCart(item)  {
   console.log(item+ " has been added to your cart.");
   return cart;
 };
+
 
 function viewCart() {
   var cartStructure = [];
@@ -48,6 +51,29 @@ function viewCart() {
   }
 };
 
-function removeFromCart() {
 
+function removeFromCart(item) {
+
+  var inCartFlag = false;
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      inCartFlag = true;
+      cart.splice(i, 1);
+    }
+  }
+  if (!inCartFlag)  {
+    console.log("That item is not in your cart.");
+  }
+  return cart;
+};
+
+
+function placeOrder(cardNumber) {
+  if (!cardNumber)  {
+    return console.log("We don't have a credit card on file for you to place your order.")
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  }
 }
