8ba271c65670f0c339dbbdc5628d001b5e849a6c
Kaige Lindberg <lindbergkaige@gmail.com>
2017-01-26 01:36:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-var cart;
-
+var cart = [];
+var cart2 = {};
+function getCart(){
+  return cart;
+}
 function setCart(newCart) {
   cart = newCart;
 }
+function addToCart(item){
+  cart.push({[item] : Math.floor(Math.random()*100)})
+  cart2[item]=cart[cart.length-1][item]
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+/* Yeah I don't get this at all.*/
+function viewCart(){
+  if (cart.length===0){
+    console.log("Your shopping cart is empty.")
+  } else {
+    const oneCost = cart2['socks']
+    const twoCost = cart2["puppy"];
+    const threeCost = cart2["iPhone"];
+    console.log(`In your cart, you have socks at $${oneCost}, puppy at $${twoCost}, iPhone at ${threeCost}.`)
+  }
+}
 
+function removeFromCart(item){
+  if (cart2.hasOwnProperty(item)) {
+    delete cart2.item
+    cart = []
+  } else {
+    console.log("That item is not in your cart.")
+
+  }
+}
 function total() {
   let t = 0
 
@@ -15,3 +44,11 @@ function total() {
 
   return t
 }
+var totalcost = total()
+function placeOrder(cardnumber){
+  if (cardnumber === undefined){
+    console.log("We don't have a credit card on file for you to place your order.")
+  } else {
+    console.log(`Your total cost is $${totalcost}, which will be charged to the card $${cardnumber}`)}
+  cart = []
+}}
