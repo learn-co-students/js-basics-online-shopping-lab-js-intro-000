1512b99089f19624267874f8ab6d9e60890f0b93
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-01-26 20:41:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-var cart;
+var cart = [];
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = (Math.random()*100).toFixed();
+  cart.push({item: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  var str = "In your cart, you have ";
+  if (cart.length > 0) {
+    for (obj of cart) {
+      for (var item in obj) {
+        str+=`${item} at ${obj[item]}, `;
+      }
+    }
+    console.log(str.slice(0,-2) +".");
+  } else {
+    console.log("Your shopping cart is empty.");
+  }
+}
+
+function removeFromCart(item) {
+  for (index of cart) {
+    
+  }
+}
+
+function placeOrder() {
+  
+}
 
 function setCart(newCart) {
   cart = newCart;
