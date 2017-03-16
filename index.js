646a1a8323887fe893f03b5afb8fb35bd032e621
Greg Benjamin <gregbenj@gmail.com>
2017-03-16 23:00:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
+}
+
+function getCart(){
+  return cart
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random()*100) + 1
+  cart.push({[item]: price})
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart(){
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  }
+  else{
+    var output = "In your cart, you have "
+    for (var index in cart){
+      for (var item in cart[index]){
+        output += `${item} at $${cart[index][item]}`
+      }
+      if (index < cart.length - 1){
+        output += ", "
+      }
+      else {
+        output += "."
+      }
+    }
+    console.log(output)
+  }
+}
+
+function removeFromCart(item){
+  var found = false
+  for (var index in cart){
+    if(cart[index].hasOwnProperty(item)){
+      cart.splice(index,1)
+      found = true
+      return cart
+    }
+  }
+  if(!found){
+    console.log("That item is not in your cart.")
+  }
+}
+
+function placeOrder(ccNumber){
+  if (ccNumber == null){
+    console.log("We don't have a credit card on file for you to place your order.")
+  }
+  else{
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${ccNumber}.`)
+  }
+  cart = []
 }
 
 function total() {
