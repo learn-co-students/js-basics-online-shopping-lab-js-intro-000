db07489a439236c9f12792bc383f4ff6ed950484
Kenneth Yung <omkeyu@gmail.com>
2017-04-20 14:00:04 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,56 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random()*100)
+ console.log(`${item} has been added to your cart.`)
+ cart.push({[item]:price})
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+if (cart.length === 0) {
+  console.log("Your shopping cart is empty.")
+}else {
+  var cartitems = 'In your cart, you have '
+for (var i = 0 ; i<cart.length ; i++) {
+  var tempkey = Object.keys(cart[i])  //temporarily store the item name 
+  var tempvalue = cart[i][tempkey] //temporarily store the item value 
+  cartitems = cartitems + `${tempkey} at $${tempvalue}${i+1===cart.length?'.' : ', '}`
+  }
+  console.log(cartitems)
+}
 }
 
 function total() {
-  // write your code here
+  var totalvalue=0
+  for (var items in cart) {
+    var tempkey = Object.keys(cart[items])  //temporarily store the item name 
+    var tempvalue = cart[items][tempkey] //temporarily store the item value 
+    totalvalue+=tempvalue
+  }
+  return totalvalue
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var i=0
+  while (i<cart.lengthcart && [i].hasOwnProperty(item)!=true) {  //as long as item number 1 doesnt match, check gain
+    i++
+  }
+  if (i===cart.length) {  //if all items are checked, than return the item is not in cart
+    console.log('That item is not in your cart.')
+  }else {
+      cart = [...cart.slice(0,i),...cart.slice(i+1)]
+      return cart
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+if (cardNumber===undefined) {
+console.log('We don\'t have a credit card on file for you to place your order.')
+}
+else  {
+console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+cart = []
+}
 }
 
