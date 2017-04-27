eb445ab3ead253f69a6a6188c347a4d6912ae24f
Valerie Corvington Farrell <vcorvington@gmail.com>
2017-04-27 01:56:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 var cart = []
 
 function getCart() {
@@ -7,14 +9,41 @@ function getCart() {
 function setCart(c) {
   cart = c
 }
-
+/*
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100) + 1;
+  for (var price in cart) {
+    console.log(`${cart[item]} has been added to your cart.`)
+  }
+  cart.push(item : price);
+}
+*/
+function addToCart(item) {
+   function getPrice(){
+   var price = Math.floor(Math.random()* 100);
+   return price;
+  }
+   cart.push({[item]:getPrice()});
+   console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  while (cart.length == 0) {
+    console.log("Your shopping cart is empty.");
+  } else { console.log(`In your cart
+  you have ${cart[item]}` + ` and ` ${cart[price]}`.`)
+  }
 }
+
+/*
+Define a function `removeFromCart` which accepts one argument, the name of the
+item you wish to remove. If the item isn't in the cart, the function should
+print out `"That item is not in your cart."`. If the item is in your cart, it
+should remove the object from the `cart` array. Then return the cart. (**HINT**:
+ Check each object's key to see if it matches the parameter, then remove it if
+ it matches.
+*/
 
 function total() {
   // write your code here
@@ -27,4 +56,3 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
-
