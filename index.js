5410a7ce266a27d97f5b9a434a6ade3c70724f7c
Valerie Corvington Farrell <vcorvington@gmail.com>
2017-05-01 21:26:56 +0000

    Done.

--- index.js
+++ index.js
@@ -6,18 +6,10 @@ function getCart() {
  return cart
 }
 
-function setCart(c) {
-  cart = c
+function setCart(newCart) {
+  cart = newCart;
 }
-/*
-function addToCart(item) {
-  var price = Math.floor(Math.random() * 100) + 1;
-  for (var price in cart) {
-    console.log(`${cart[item]} has been added to your cart.`)
-  }
-  cart.push(item : price);
-}
-*/
+
 function addToCart(item) {
    function getPrice(){
    var price = Math.floor(Math.random()* 100);
@@ -29,30 +21,54 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  while (cart.length == 0) {
+  if (cart.length === 0) {
     console.log("Your shopping cart is empty.");
-  } else { console.log(`In your cart
-  you have ${cart[item]}` + ` and ` ${cart[price]}`.`)
+  } else {
+
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+        }
+    }
+   console.log("In your cart, you have " + items.join(", ") + ".");
+}
+}
+
+
+function removeFromCart(item) {
+  var itemInCart =
+  false
+  for(var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      let index = cart.lastIndexOf(item)
+      cart.splice(index, 1)
+      itemInCart = true
+      return cart;
   }
 }
 
-/*
-Define a function `removeFromCart` which accepts one argument, the name of the
-item you wish to remove. If the item isn't in the cart, the function should
-print out `"That item is not in your cart."`. If the item is in your cart, it
-should remove the object from the `cart` array. Then return the cart. (**HINT**:
- Check each object's key to see if it matches the parameter, then remove it if
- it matches.
-*/
-
-function total() {
-  // write your code here
+if (itemInCart === false) {
+  console.log("That item is not in your cart.")
+}
 }
 
-function removeFromCart(item) {
-  // write your code here
-}
+
+function total(item[i]) {
+var t =  0;
+           for (var i = 0; i < cart.length; i++) {
+               for (t += Number(item[i]);
+                }
+            return t;
+           }
+
+
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(!cardNumber) {
+    console.log("We don't have a credit card on file for you to place your order.")
+  } else {
+    console.log(`"Your total cost is $${total()}, which will be charged to the card ${cardNumber}."`)
+  }
+  cart = [];
 }
