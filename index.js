6be07e483d105f9b7fbf9c1eb121872035d4665b
Stanislav Zhukovskiy <menistrel2005@yandex.ru>
2017-05-03 21:58:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart = []
+var cart = [];
 
 function getCart() {
  return cart
@@ -9,22 +9,83 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var price = Math.floor(Math.random()*100, -2);
+var string = `${item} has been added to your cart.`;
+console.log(string);
+var object = {};
+object[item] = price;
+cart.push(object);
+
+
+return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var string = 'In your cart, you have ';
+  var cartItem;
+
+  if (!cart.length) {
+      console.log('Your shopping cart is empty.');
+  }
+  else {
+      for(var i = 0; i < cart.length; i++) {
+
+      cartItem = cart[i];
+
+  		for (var keys in cartItem) {
+          if (i == cart.length - 1) {
+              string += keys + " at $" + cartItem[keys] + ".";
+              console.log(string);
+          }
+          else {
+              string += keys + " at $" + cartItem[keys] + ", ";
+          }
+
+  			}
+
+    }
+
+  }
+
 }
 
 function total() {
-  // write your code here
+    var total = 0;
+
+        for(var i = 0; i < cart.length; i++) {
+        var cartItem;
+
+        cartItem = cart[i];
+
+        for (var keys in cartItem) {
+            total += cartItem[keys];
+          }
+        }
+    return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+var cartItem;
+      for(var i = 0; i < cart.length; i++) {
+
+        cartItem = cart[i];
+
+          if (cartItem.hasOwnProperty(item)) {
+            cart.pop();
+            return cart;
+          }
+      }
+      console.log('That item is not in your cart.');
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
-}
+  var string = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  if(!cardNumber) {
+    console.log('We don\'t have a credit card on file for you to place your order.');
+  }
+  else {
 
+    console.log(string);
+    cart.pop();
+  }
+}
