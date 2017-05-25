b28bc0e1b478755ec5387d2ba3693d7cd4c2da44
Krista Abraham <krista.abraham@gmail.com>
2017-05-25 21:27:07 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,45 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var temp = {};
+  temp[item] = Math.floor(Math.random() * 100);
+  cart.push(temp);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log('Your shopping cart is empty.');
+} else {
+    var result = `In your cart, you have`;
+    for (var i = 0; i < cart.length; i++) {
+      var itemName = Object.keys(cart[i])[0];
+      result += ` ${itemName} at $${cart[i][itemName]},`;
+  }
+    result = result.slice(0, result.length-1);
+    result = result += ".";
+}
+  console.log(result);
 }
 
 function total() {
-  // write your code here
+  var tot = 0;
+  for (var i = 0; i < cart.length; i++) {
+    var itemName = Object.keys(cart[i])[0];
+    tot += cart[i][itemName];
+  }
+  return tot;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    var itemName = Object.keys(cart[i])[0];
+    if (itemName === item) {
+      delete Object.keys(cart[i]([0];
+    }
 }
 
 function placeOrder(cardNumber) {
   // write your code here
 }
-
