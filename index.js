2d6ac8decb52cd4a9c2f388bf990dc7a1124edba
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-07-02 19:46:34 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,70 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({ [item]: Math.floor(Math.random() * 100) });
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  }
+  var sentence = `In your cart, you have `;
+  var i = 0;
+  for (i = 0; i < cart.length; i++) {
+    var item = Object.keys(cart[i])[0];
+    var price = cart[i][item]
+    if (cart.length === 1) {
+      sentence += `${item} at $${price}.`;
+    } else if (cart.length === 2) {
+      if (i === 0) {
+        sentence += `${item} at $${price} `;
+      } else {
+        sentence += `and ${item} at $${price}.`;
+      }
+    } else if (cart.length > 2) {
+      if (i < cart.length - 1) {
+        sentence += `${item} at $${price}, `;
+      } else {
+        sentence += `and ${item} at $${price}.`;
+      }
+    }
+  }
+  return sentence;
 }
 
 function total() {
-  // write your code here
+  var totalPrice = 0;
+  var i = 0;
+  for (i = 0; i < cart.length; i++) {
+    var item = Object.keys(cart[i])[0];
+    var price = cart[i][item]
+    totalPrice = totalPrice + price;
+  }
+  return totalPrice;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var i = 0;
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty([item])) {
+      cart.splice(i,1);
+      return cart;
+    } else {
+      null;
+    }
+  }
+  return `That item is not in your cart.`;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  purchaseStatement = "";
+  if (!cardNumber) {
+    purchaseStatement += `Sorry, we don\'t have a credit card on file for you.`;
+  } else {
+    purchaseStatement += `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
+  }
+  return purchaseStatement;
 }
