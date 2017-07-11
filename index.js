f4ba7fa0a6062c9c80b0c4c90821c32049a76ef5
sumanth Jesta <sumanthjesta14@gmail.com>
2017-07-11 18:20:51 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,90 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  const ss= item;
+ cart.push({[ss]:Math.floor((Math.random() * 100) + 1)});
+ console.log(item+" has been added to your cart.");
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length<=0)
+    console.log("Your shopping cart is empty.");
+  else {
+    var s="In your cart, you have ";
+    var lp=[];
+    var vals=[];
+     var ks=[];
+     for(var w=0;w<cart.length;w++)
+           ks[w]=Object.keys(cart[w]);
+     for(var a=0;a<ks.length;a++)
+         vals[a]=cart[a][ks[a]];
+
+    for(var key in cart)
+    {
+      lp.push(ks[key]+" at $"+vals[key]);
+
+    }
+    if(cart.length==1)
+      console.log(s+lp[0]+".");
+    if(cart.length==2)
+      console.log(s+lp[0]+" and "+lp[1]+".");
+    if(cart.length>=3)
+    {
+      var tt="";
+      for(var k=0;k<lp.length;k++)
+      {
+        if(k==lp.length-1)
+          tt=tt+"and "+lp[k]+".";
+        else
+        tt=tt+lp[k]+", ";
+      }
+      console.log(s+tt);
+    }
+  }
 }
 
 function total() {
-  // write your code here
+  var tot=0;
+  var vals=[];
+   var ks=[];
+   for(var w=0;w<cart.length;w++)
+         ks[w]=Object.keys(cart[w]);
+   for(var a=0;a<ks.length;a++)
+   {
+       vals[a]=cart[a][ks[a]];
+       tot=tot+vals[a]
+    }
+  return tot;
 }
 
 function removeFromCart(item) {
-  // write your code here
-}
+  var flg=true;
+  for(var i=0;i<cart.length;i++)
+  {
+    if (cart[i].hasOwnProperty(item)) {
+          flg=false;
+          if(item==Object.keys(cart[i]))
+          {
+              cart.splice(i,1);
+
+            }
+
+    }
+  }
+    if(flg) {
+      console.log("That item is not in your cart.");
+    }
+
+    return cart;
+
+  }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(!cardNumber)
+    console.log("Sorry, we don't have a credit card on file for you.");
+    else {
+      console.log("Your total cost is $"+total()+", which will be charged to the card "+cardNumber+".");
+      cart=[];
+    }
 }
