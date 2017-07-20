1b816fc34f7799ce70da8d079598dfbf81a5e027
Annette Maza <nettymaza@gmail.com>
2017-07-20 21:39:38 +0000

    Done.

--- index.js
+++ index.js
@@ -78,11 +78,10 @@ function formatItem(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (!cardNumber) {
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  } else {
     console.log("Sorry, we don't have a credit card on file for you.")
-    return;
   }
-
-  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-  cart = [];
 }
