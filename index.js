e3c00991b343f5a30403ce69b213422644a9ff93
Ramy Khorshed <ramykhorshed@gmail.com>
2017-08-07 10:32:52 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,66 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor((Math.random() * 100) + 1);
+ var itemName = item.toString();
+ var itemObject = {[itemName]:price};
+ cart.push(itemObject)
+ console.log(`${itemName} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+     console.log("Your shopping cart is empty.")
+   }
+   else if (cart.length === 2) {
+     var items = [];
+     for (var i = 0; i < cart.length; i++) {
+       for (var item in cart[i]) {
+         items.push(item + " at $" + cart[i][item])
+       }
+     }
+     console.log("In your cart, you have " + items.join(" and ") + ".");
+   }
+   else {
+     var items = [];
+     for (var i = 0; i < cart.length-1; i++) {
+       for (var item in cart[i]) {
+         items.push(item + " at $" + cart[i][item])
+       }
+     }
+     for (var i = cart.length; i < cart.length; i++) {
+       for (var item in cart[i]) {
+         items.push(" and"+ item + " at $" + cart[i][item])
+       }
+     }
+
+     console.log("In your cart, you have " + items.join(", ") + ".");
+   }
+}
+
+function poopCart() {
+  if (cart.length>0) {
+      var statement = "In your cart, you have"
+      for (var i = 0; i < cart.length; i++) {
+        var localObject = cart[i];
+        var localName = Object.keys(localObject);
+       	localName = localName.toString();
+        var localPrice = localObject[localName];
+        var addition = ` ${localName} at $${localPrice}`;
+        var statement = statement + addition;
+        if (i === cart.length-1) {
+          statement= `${statement}.`;
+        }
+        else {
+          statement= `${statement},`;
+        }
+      }
+    }
+  else {
+    var statement = "Your shopping cart is empty"
+  }
+    return statement;
 }
 
 function total() {
