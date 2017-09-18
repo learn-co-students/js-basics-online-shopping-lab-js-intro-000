a03e8d74753e1af86e931987caf1e1862da869d9
Garry Clerge <clergeg@gmail.com>
2017-09-18 04:25:34 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,81 @@ function setCart(c) {
   return cart;
 }
 
+
 function addToCart(item) {
- // write your code here
+  let itemName = item;
+
+  function itemPrice (min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  cart.push({[itemName]: itemPrice(1,100)});
+
+  console.log(`${itemName} has been added to your cart.`)
+
+  return cart;
 }
+
 
 function viewCart() {
-  // write your code here
+  var fullcart = "In your cart, you have";
+
+    for (let i = 0; i < cart.length; i++){
+      var keys = Object.keys(cart[i]);
+      var values = (cart[i][keys]);
+
+      if ((cart[cart.length - 1] === cart[i]) && (cart.length === 1)){
+        fullcart += ` ${keys} at $${values}.`;
+
+      } else if ((cart[cart.length - 1] === cart[i]) && (cart.length > 1)){
+          fullcart += ` and ${keys} at $${values}.`;
+
+        } else if (cart.length === 2 && cart[cart.length - 2] === cart[i]) {
+          fullcart += ` ${keys} at $${values}`;
+
+          } else { fullcart += ` ${keys} at $${values},`;
+            }
+    }
+
+    if (cart.length === 0){console.log ("Your shopping cart is empty.");
+
+    } else {console.log(fullcart)
+      }
 }
+
 
 function total() {
-  // write your code here
+  var totalCost = 0;
+
+  for(let i = 0; i < cart.length; i++){
+
+    var keys = Object.keys(cart[i]);
+    var values = (cart[i][keys]);
+
+    totalCost += values;
+  }
+
+return totalCost;
 }
+
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++){
+
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+
+  console.log( "That item is not in your cart.");
 }
 
+
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber){
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  } else { console.log ("Sorry, we don't have a credit card on file for you.");
+    }
 }
