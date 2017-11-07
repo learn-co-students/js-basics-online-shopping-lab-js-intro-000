f09870d99579c91a2c5c158ea44c4754a1cde789
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-07 17:46:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cart = [];
+var final_price;
 
 function getCart() {
  return cart;
@@ -10,21 +11,96 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+
+  var rand = Math.random() * 100;
+  var price = Math.floor(rand);
+
+  var product = {[item]:price};
+  cart.push(product);
+
+  console.log(`${item} has been added to your cart.`);
+
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var liste = [];
+  var final = [];
+
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  }
+
+  else if (cart.length === 1) {
+    for (var elem in cart) {
+      for (var key in cart[elem]) {
+        console.log(`In your cart, you have ${key} at $${cart[elem][key]}.`);
+        }
+      }
+    }
+
+  else if (cart.length === 2){
+    for (var elem in cart) {
+      for (var key in cart[elem]) {
+        liste.push(`${key} at $${cart[elem][key]}`)
+      }
+    }
+    console.log(`In your cart, you have ${liste[0]} and ${liste[1]}.`);
+  }
+
+  else {
+    for (var elem in cart) {
+      for (var key in cart[elem]) {
+        liste.push(`${key} at $${cart[elem][key]}`)
+      }
+    }
+    for (var i = 0; i < liste.length; i++) {
+      if (i == 0) {
+        final.push(`In your cart, you have ${liste[i]}`);
+      }
+      else if (i > 0 && i < liste.length-1){
+        final.push(`, ${liste[i]}`);
+      }
+      else if (i == liste.length-1) {
+        final.push(`, and ${liste[i]}.`);
+      }
+    }
+    var text = final.reduce(function(ant,atu,index,tudo){
+      return ant + atu;
+    })
+    console.log(text);
+  }
+
 }
 
 function total() {
-  // write your code here
+  final_price = 0;
+  for (var elem in cart) {
+    for (var key in cart[elem]) {
+      final_price += cart[elem][key];
+    }
+  }
+  return final_price;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  //for(var i=0; i < cart.length; i++){
+  for (var i in cart) {
+    if (cart[i].hasOwnProperty(item)){
+      cart.splice(i,1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  var tot = 0;
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  } else {
+    console.log(`Your total cost is $${final_price}, which will be charged to the card ${cardNumber}.`);
+    cart.pop();
+    return final_price;
+  }
 }
