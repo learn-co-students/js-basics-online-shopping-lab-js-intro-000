652bcdd4dc9718560855e9c9a79ede51c5e5f55d
Kathy Hudson <khitx@icloud.com>
2017-11-22 23:26:31 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,72 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.round(Math.random() * (100 - 1) + 1)
+  var obj = {}
+  Object.assign(obj, {[item] : price})
+  //Object.assign(cart, {[item] : price})
+  cart.push(obj)
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  var str = 'In your cart, you have'
+
+  if (cart.length === 0) {
+    str = 'Your shopping cart is empty.'
+  }
+  else if (cart.length === 1) {
+	str = `${str} ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`
+  }
+  else if (cart.length === 2) {
+ 	for (var i = 0; i < cart.length - 1; i++) {
+	  str = `${str} ${Object.keys(cart[i])} at $${Object.values(cart[i])}`
+	}
+	str = `${str} and ${Object.keys(cart[i])} at $${Object.values(cart[i])}.`
+  }
+  else {
+ 	for (var i = 0; i < cart.length - 1; i++) {
+	  str = `${str} ${Object.keys(cart[i])} at $${Object.values(cart[i])},`
+	}
+	str = `${str} and ${Object.keys(cart[i])} at $${Object.values(cart[i])}.`
+  }
+  console.log(str.slice(0,-1) + '.')
 }
 
 function total() {
-  // write your code here
+  	var t = 0
+	for (var i = 0; i < cart.length; i++) {
+	  t = t + Number(Object.values(cart[i]))
+	}
+	return t
 }
 
 function removeFromCart(item) {
-  // write your code here
+	var str = ''
+
+	for (var i = 0; i < cart.length;i++) {
+		var obj = {}
+		str = cart[i]
+		console.log(str)
+		Object.assign(obj, str)
+		console.log(obj)
+		if (obj.hasOwnProperty(item)) {
+			console.log(obj.hasOwnProperty(item))
+			cart.splice(i,1)
+      return cart
+    }
+  }
+  console.log('That item is not in your cart.')
+  return cart
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  }
 }
