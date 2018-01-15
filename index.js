776e1e3d0e206addc6da96e335a08f026ebc8fd4
nancy sin <nsin@sbcglobal.net>
2018-01-15 03:41:36 +0000

    Done.

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ function removeFromCart(item) {
  else {
   console.log(`That item is not in your cart.`);
 }
+}
 
 function placeOrder(cardNumber) {
  if (cardNumber === undefined) {
@@ -69,4 +70,4 @@ function placeOrder(cardNumber) {
    return cart
  }
  }
-}
+ 
