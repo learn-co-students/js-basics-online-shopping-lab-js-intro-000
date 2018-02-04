b3453ace5ce695732f19f40ef892006b8b9e01c8
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-02-04 18:50:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -10,21 +10,61 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({
+    [item]: Math.floor(Math.random() * 100) + 1
+  });
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var result = 'In your cart, you have ';
+  if (cart.length === 0) {
+    console.log('Your shopping cart is empty.');
+  } else {
+    for (var i = 0; i < cart.length; i++) {
+      if (i === (cart.length - 1)) {
+        result += `${Object.keys(cart[i])} at $${Object.values(cart[i])}.`;
+      } else if (i === (cart.length - 2)) {
+        result += cart.length === 2 ? `${Object.keys(cart[i])} at $${Object.values(cart[i])} and ` : `${Object.keys(cart[i])} at $${Object.values(cart[i])}, and `;
+      } else {
+        result += `${Object.keys(cart[i])} at $${Object.values(cart[i])}, `;
+      }
+    }
+    console.log(result);
+  }
 }
 
 function total() {
-  // write your code here
+  var totalCost = 0;
+  for (var i = 0; i < cart.length; i++) {
+    totalCost += parseInt(Object.values(cart[i]));
+  }
+  return totalCost;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var result = cart.length;
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item) === true) {
+      cart.splice(i, 1);
+    } else {
+      result--;
+    }
+  }
+  if (result === 0) {
+    console.log('That item is not in your cart.');
+  } else {
+    return cart;
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  var cost = total();
+  if (cardNumber === undefined) {
+    console.log('Sorry, we don\'t have a credit card on file for you.');
+  } else {
+    console.log(`Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`);
+  }
+  cart = [];
 }
