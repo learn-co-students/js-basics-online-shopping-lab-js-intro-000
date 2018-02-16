816197f806234d45c4544e190e60a915e3af5b31
Juan David Bermudez Celedon <juanbermucele@hotmail.com>
2018-02-16 18:21:04 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,70 @@ function setCart(c) {
   return cart;
 }
 
+function randomPrice() {
+  return parseInt(Math.random() * 100);
+}
+
 function addToCart(item) {
- // write your code here
+  cart.push({
+   [item]: randomPrice()
+ });
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else if (cart.length === 1) {
+    let k = Object.keys(cart[0])[0];
+    console.log(`In your cart, you have ${k} at $${cart[0][k]}.`);
+  } else if (cart.length === 2) {
+      let k = Object.keys(cart[0])[0];
+      let k2 = Object.keys(cart[1])[0];
+    console.log(`In your cart, you have ${k} at $${cart[0][k]} and ${k2} at $${cart[1][k2]}.`);
+  } else {
+    let text = "In your cart, you have ";
+    for (let i = 0; i < cart.length; i++) {
+      let k = Object.keys(cart[i])[0];
+      if (i != cart.length - 1) {
+        text += `${k} at $${cart[i][k]}, `;
+      } else {
+        text += `and ${k} at $${cart[i][k]}.`;
+      }
+    }
+    console.log(text);
+    //let k = keys[length - 1];
+    //text += `and ${keys[k]} at $${cart[length - 1][k]}.`;
+  }
 }
 
 function total() {
-  // write your code here
+  let total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    total += Object.values(cart[i])[0];
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber != null || cardNumber != undefined) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    while(cart.length > 0) {
+      cart.pop();
+    }
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
 }
