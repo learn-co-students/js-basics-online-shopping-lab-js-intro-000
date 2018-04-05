94da6a061b3eaaf58de608d946ec0b0b6c3592b0
Christopher Agius <chrisagius@fastmail.com>
2018-04-05 17:42:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var cart = [];
 
+/* Function used from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+   section entitled "Getting a random integer between two values, inclusive" */
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
 function getCart() {
  return cart;
 }
@@ -10,21 +18,53 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ let itemObj = new Object();
+ itemObj.itemName = item;
+ itemObj.itemPrice = getRandomIntInclusive(1, 100);
+ cart.push(itemObj);
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at \$${cart[0].itemPrice}.`;
+  } else {
+    let cartString = `In your cart, you have ${cart[0].itemName} at \$${cart[0].itemPrice}`;
+    for (let i = 1; i <= cart.length - 2; i++) {
+      cartString += `, ${cart[i].itemName} at \$${cart[i].itemPrice}`;
+    }
+    cartString += `, and ${cart[cart.length - 1].itemName} at \$${cart[cart.length - 1].itemPrice}.`;
+    return cartString;
+  }
 }
 
 function total() {
-  // write your code here
+  let runningTotal = 0;
+  for (let i = 0; i < cart.length; i++) {
+    runningTotal += cart[i].itemPrice;
+  }
+  return runningTotal;
 }
 
+
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    return 'Sorry, we don\'t have a credit card on file for you.';
+  } else {
+    let checkoutStr = `Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
+    return checkoutStr;
+  }
 }
