ae57d1b20b0d94245f824b578d561cf989c9ad28
Jonathan Hosein <jonathan.hosein@live.com>
2018-06-08 03:25:13 +0000

    Done.

--- index.js
+++ index.js
@@ -20,16 +20,40 @@ function addToCart(item) {
 
 }
 
-function viewCart() {
-  // write your code here
+function viewCart() {//credit to arellaEG because the learn IDE lost my code on this TWICE!
+  if (cart.length===0){
+    str= 'Your shopping cart is empty.'
+  }
+  else {
+    var str=`In your cart, you have `;
+    if (cart.length===1){
+       str=str + `${cart[0]['itemName']} at $${cart[0]['itemPrice']}.`}
+     else {
+       for (var i=0; i<cart.length-1; i++){
+      str+= `${cart[i]['itemName']} at $${cart[i]['itemPrice']}, `}
+    str+=`and ${cart[i]['itemName']} at $${cart[i]['itemPrice']}.`
+  }}
+  return str
 }
 
 function total() {
-  // write your code here
+  let total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
+  }
+
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+
+    if (item === cart[i].itemName)
+    {
+      cart.splice(i,1);
+    }
+    return cart;
+  }
 }
 
 function placeOrder(cardNumber) {
