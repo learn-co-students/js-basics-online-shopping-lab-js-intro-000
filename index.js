068d9b66b23f77826f00ed68a77ed41b3350f637
Erika Jane <erika.dauria@gmail.com>
2018-06-10 00:15:43 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100);
+  var newItem = {
+    itemName: item,
+    itemPrice: price,
+  };
+  cart.push(newItem);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
