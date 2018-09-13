dbafa4ec828bab6dfa1975929e5403f82e16dabc
Johnathon Wood <jaywood128@gmail.com>
2018-09-13 17:52:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart = [];
+var cart = [{ cat : 1}, {dog: 2}, {fish: 3}, {otter: 5}];
 
 function getCart() {
  return cart;
@@ -10,12 +10,40 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  const price = Math.floor(Math.random() * 100+1);
+   cart.push({itemName: item, itemPrice : price} );
+   return `${item} has been added to your cart.`
+
 }
 
 function viewCart() {
-  // write your code here
-}
+  if (cart === 0) {
+    console.log("Your shopping cart is empty.")
+  }
+  var newarray = [];
+   for (var i = 0; i < cart.length; i++) {
+     var keys = Object.keys(cart[i])[0]
+     newarray.push(keys + "at $" + cart[i][keys])
+
+
+   }
+     var mystring = "In your cart, you have "
+      if (newarray.length === 1) {
+        mystring += newarray + "."
+      } else if (newarray.length === 2) {
+        mystring += (newarray[0] + " and " + newarray[1] + ".")
+      } else if (newarray.length > 2) {
+        var lastelement = newarray.pop()
+        var other_item = newarray.join(", ")
+        mystring += (other_item + ", and " + lastelement + ".")
+      }
+        console.log(mystring)
+  }
+
+
+
+
+
 
 function total() {
   // write your code here
