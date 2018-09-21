d9d377b3bb1254ad144a793fbe8a297dca4ee0bf
Aaron Bougouneau <a.bougouneau@gmail.com>
2018-09-21 11:44:44 +0000

    Done.

--- index.js
+++ index.js
@@ -96,7 +96,6 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
 
     if (typeof cardNumber == 'undefined'){
       return 'Sorry, we don\'t have a credit card on file for you.'
