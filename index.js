0cc2fde9e4cc2b12e4e38620ba825065adc097f0
danieldjordan <danieldjordan@gmail.com>
2018-10-16 15:31:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var cart = [];
 
-function getCart() {
+function getCart(item) {
+
  return cart;
 }
 
@@ -10,21 +11,69 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let price = (Math.floor(Math.random() * 100))
+  var itemVar = {itemName: item, itemPrice: price};
+  console.log(itemVar)
+  cart.push(itemVar)
+  return `${itemVar.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+  if(cart.length > 0){
+    var message = 'In your cart, '
+    for(var i = 0; i<cart.length; i++){
+      if(i === 0){
+        message += `you have ${cart[i].itemName} at $${cart[i].itemPrice}`
+      }
+      else if(i == (cart.length-1)&&(cart.length>1)){
+        message += `, and ${cart[i].itemName} at $${cart[i].itemPrice}`
+      }
+      else{
+        message += `, ${cart[i].itemName} at $${cart[i].itemPrice}`
+      }
+    }
+    message += '.'
+  }
+  else{
+    message = 'Your shopping cart is empty.'
+  }
+  return message
+ }
+ 
+ 
+ function total(){
+   var totalPrice = 0;
+   for(var i = 0; i<cart.length; i++){
+     var itemPrice = Number(cart[i].itemPrice)
+     totalPrice = itemPrice + totalPrice
+   }
+   return totalPrice;
+ }
+ 
+  function removeFromCart(item){
+   var found = false
+   for(var i = 0; i<cart.length; i++){
+     if(item == cart[i].itemName){
+       cart.splice(i, 1);
+       found = true
+     }
+   }
+   if(found===false){
+     return 'That item is not in your cart.'
+    }
+    else{
+      return cart;
+    }
+  }
+  
+  function placeOrder(ccNumber){
+    if(ccNumber){
+      var totalPrice = total()
+      cart = [];
+      var message = `Your total cost is $${totalPrice}, which will be charged to the card ${ccNumber}.` 
+      return message;
+    }
+    else{
+      return "Sorry, we don't have a credit card on file for you." 
+    }
+  }
