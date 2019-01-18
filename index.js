ffeb9d62defff61905705c5bdadc7b181335abc7
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-18 20:31:08 +0000

    Done.

--- index.js
+++ index.js
@@ -9,21 +9,94 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart( item ) {
+  var itemDetails = getItemDetails( item )
+  getCart().push( itemDetails );
+
+  return `${ itemDetails.itemName } has been added to your cart.`
+}
+
+function getItemDetails( itemInfo ) {
+  return {
+    itemName: itemInfo,
+    itemPrice: Math.floor( Math.random() * 100 ) + 1
+  }
 }
 
 function viewCart() {
-  // write your code here
+  return getCart().length < 1 ? "Your shopping cart is empty." : listCartItems();
+}
+
+function listCartItems() {
+  var cartMessage = 'In your cart, you have '
+
+  if ( getCart().length >=  1 ) {
+    cartMessage += `${ getCart()[ 0 ].itemName } at $${ getCart()[ 0 ].itemPrice }`
+  }
+  if ( getCart().length >= 2 ) {
+    var middleCartMessage = '';
+
+    for ( var i = 1; i < getCart().length - 1; i++ ) {
+      middleCartMessage += `, ${ getCart()[ i ].itemName } at $${ getCart()[ i ].itemPrice }`
+    }
+    cartMessage += `${ middleCartMessage}, and ${ getCart()[ i ].itemName } `
+    cartMessage += `at $${ getCart()[ i ].itemPrice }`
+  }
+
+  return `${ cartMessage }.`;
 }
 
 function total() {
-  // write your code here
+  var totalCost = 0;
+
+  for (var i = 0; i < getCart().length; i++) {
+    totalCost += getCart()[ i ].itemPrice;
+  }
+
+  return totalCost;
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart( item ) {
+  var itemToRemove;
+
+  if ( item ) {
+    for ( var i = 0; i < getCart().length; i++ ) {
+      if ( getCart()[ i ].itemName === item ) {
+        itemToRemove = getCart()[ i ];
+        var index = getCart().indexOf( itemToRemove );
+        cart.splice( index, 1 )
+      }
+      else {
+        return "That item is not in your cart."
+      }
+    }
+    return cart;
+  }
 }
+
+// function removeFromCart(item) {
+  // var itemToRemove = findItem( item );
+  // return itemToRemove ? removeItem( itemToRemove ) : "That item is not in your cart."
+// }
+
+// function findItem(item) {
+//   var itemToRemove;
+//
+//   if ( item ) {
+//     for ( var i = 0; i < getCart().length; i++ ) {
+//       if ( getCart()[ i ].itemName === item ) {
+//         itemToRemove = getCart()[ i ];
+//       }
+//     }
+//   }
+//
+//   return itemToRemove;
+// }
+//
+// function removeItem(itemToRemove) {
+//   var index = getCart().indexOf(itemToRemove);
+//   getCart().splice(index, 1)
+// }
 
 function placeOrder(cardNumber) {
   // write your code here
