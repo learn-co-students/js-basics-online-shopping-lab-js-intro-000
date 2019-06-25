6953bfa5ad7ed0c60a051db684bf44e36df089eb
Hema Venkata <hvenkata@live.com>
2019-06-25 01:55:58 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,62 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+  var newAddition = {itemName: item, itemPrice: getRandomInt(1,100)};
+  cart.push(newAddition)
+  console.log(cart)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length == 1) {
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
+} else if (cart.length == 2) {
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
+} else if (cart.length > 2) {
+  var midsentence = ``
+    for (let i = 0; i < getCart().length-1; i++) {
+       midsentence += `${getCart()[i].itemName} at $${getCart()[i].itemPrice}, `
+//    return `In your cart, you have ${getCart()[i].itemName} at $${getCart()[i].itemPrice},`
+    }
+    return `In your cart, you have ${midsentence}and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}.`
+
+} else {
+  return "Your shopping cart is empty."
+}
 }
 
 function total() {
-  // write your code here
+  var sum = 0;
+  for (let i = 0; i < getCart().length; i++) {
+    sum = sum + getCart()[i].itemPrice;
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
-}
+//  var removed
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+        return "That item is not in your cart."
+    }
 
 function placeOrder(cardNumber) {
-  // write your code here
+//    var freshArray = [];
+    if (cardNumber === undefined) {
+      return "Sorry, we don't have a credit card on file for you."
+    } else {
+      return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    setCart([])
+  //  cart.splice(0, cart.length)
+  }
+  //return cart
 }
