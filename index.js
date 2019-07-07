e77063b6006f21b6f122f7aef109281c18e27fb7
Donelle Reed <donellereed@gmail.com>
2019-07-07 17:03:51 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,94 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var cartItem =
+  {itemName:item,
+  itemPrice: Math.floor((Math.random()* 10)+ 1)
+}
+cart.push(cartItem)
+return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+var itemsAndPrices = []
+
+  for (var i = 0; i <= cart.length - 1; i ++){
+  var itemAndPrice = []
+    Object.entries(cart[i]).forEach(([key, value]) => itemAndPrice.push(value))
+  	itemsAndPrices.push(itemAndPrice);
+    }
+
+var itemContents = []
+
+  if (itemsAndPrices.length <= 2){
+    for (var i = 0; i <= itemsAndPrices.length -1; i ++){
+    itemContents.push(`${(itemsAndPrices[i])[0]} at $${(itemsAndPrices[i])[1]}`)
+    }
+  }else {
+    for (var i = 0; i <= itemsAndPrices.length -1; i ++){
+    itemContents.push(` ${(itemsAndPrices[i])[0]} at $${(itemsAndPrices[i])[1]}`)
+    }
+  }
+
+
+    if (itemContents.length === 0){
+    return "Your shopping cart is empty."
+    } else if (itemContents.length === 1) {
+    return `In your cart, you have ${itemContents}.`
+  } else if (itemContents.length === 2){
+    return [`In your cart, you have ${itemContents.slice(0, itemContents.length - 1)}, and ${itemContents.slice(-1)}.`]
+    } else if (itemContents.length >= 3){
+    return [`In your cart, you have${itemContents.slice(0, itemContents.length - 1)}, and${itemContents.slice(-1)}.`]
+    }
 }
+
 
 function total() {
-  // write your code here
+  var itemsAndPrices = []
+
+  for (var i = 0; i <= cart.length - 1; i ++){
+  var itemAndPrice = []
+    Object.entries(cart[i]).forEach(([key, value]) => itemAndPrice.push(value))
+  	itemsAndPrices.push(itemAndPrice);
+    }
+
+var sum = 0
+
+  for (var i = 0; i <= itemsAndPrices.length -1; i ++){
+  sum += (itemsAndPrices[i])[1]
+  }
+return sum
 }
 
-function removeFromCart(item) {
-  // write your code here
+
+function removeFromCart(item){
+var counter = 1
+var notDeleted = "not deleted"
+        for (var i = 0; i <= cart.length - 1; i ++){
+                Object.entries(cart[i]).forEach(([key, value]) => {
+
+                   if (item === value){
+                     return cart.splice(i, 1)
+                  } else if (counter === 2*(cart.length)){
+                      return notDeleted = "That item is not in your cart."
+
+                  } else {
+                    counter += 1
+                  }
+                })
+        }
+  if (notDeleted === "That item is not in your cart."){
+  return notDeleted
+  }
 }
+
 
 function placeOrder(cardNumber) {
-  // write your code here
+if (typeof cardNumber === 'undefined'){
+  return 'Sorry, we don\'t have a credit card on file for you.'
+} else {
+    var message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    cart = []
+    return message
+  }
 }
