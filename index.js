ccad72192d9ccb064273771e853867c8c2bd2d3e
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-24 07:21:39 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,62 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+ function addToCart(item) {
+ var entry = {};
+ var num = (Math.random() * 99) + 1;
+ var price = Math.ceil(num)
+ var lit1;
+ Object.assign(entry, {["itemName"]: item, ["itemPrice"]: price});
+ cart.push(entry);
+ lit1 = item + " has been added to your cart.";
+ return lit1
 }
 
 function viewCart() {
-  // write your code here
+var lit2;
+var occ = 0;
+var common = cart.length - 2
+  if (cart.length === 0) {
+    lit2 = "Your shopping cart is empty." }
+  else  if (cart.length === 1) {
+    lit2 = "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice +"." }
+  else { lit2 = "In your cart, you have "
+    while (occ <= common) {
+        lit2 = lit2 + cart[occ].itemName + " at $" + cart[occ].itemPrice + ", ";
+        occ = occ + 1}
+    lit2 = lit2 + "and " + cart[occ].itemName + " at $" + cart[occ].itemPrice + "."}
+  return lit2
 }
 
 function total() {
-  // write your code here
+var tot = 0
+  for (let occ = 0; occ < cart.length; occ++) {
+     tot = tot + cart[occ].itemPrice}
+  return tot
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var lit3;
+  var occ = 0;
+  while (occ <= (cart.length - 1) && item != cart[occ].itemName) {
+     occ = occ + 1}
+  if (occ === cart.length) {
+     lit3 = "That item is not in your cart.";
+     return lit3}
+  else { cart = [...cart.slice(0,occ), ...cart.slice(occ+1)];
+    return cart}
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+var lit4;
+var check = cardNumber;
+console.log(check);
+  if (check === undefined) {
+    lit4 = "Sorry, we don't have a credit card on file for you."
+  } else {
+    var tot = total();
+    var lit4 = "Your total cost is $" + tot + ", which will be charged to the card " + cardNumber + ".";
+    cart = []
+  }
+  return lit4
 }
