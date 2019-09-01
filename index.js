070de5306e14603b960254be13307599d2ad9cce
Sunny Niu <sunnyyingli@gmail.com>
2019-09-01 03:38:25 +0000

    Done.

--- index.js
+++ index.js
@@ -23,8 +23,6 @@ function viewCart() {
   if(cart.length > 0){
     if(cart.length === 1){
       return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
-    }else if(cart.length === 2){
-      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
     }else{
       let preMessage = `In your cart, you have`
       let itemList = [];
