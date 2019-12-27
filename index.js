78ebc064a460926a54be992ce22e41143fe403d3
Aylin Demir <aylin.de@outlook.com>
2019-12-27 21:25:00 +0000

    Done.

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ function removeFromCart(name) {
     for(let i =0;i<cart.length;i++){
       if(cart[i].itemName === name){
          cart.splice(i,1)
-         learn save
       }else{
         return "That item is not in your cart."
       }
@@ -57,6 +56,6 @@ function placeOrder(cardNumber) {
   }else{
     var totalCost = total()
     cart = [];
-    return `Your total cost is ${totalCost}, which will be charged to the car ${cardNumber}`
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
   }
 }
