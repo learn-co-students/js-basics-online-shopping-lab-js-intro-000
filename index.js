4cb9cb90ae4293857ec56853e4e5e1aba40c6cc7
Aylin Demir <aylin.de@outlook.com>
2019-12-27 21:37:16 +0000

    Done.

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ return totalPrice
 function removeFromCart(name) {
     for(let i =0;i<cart.length;i++){
       if(cart[i].itemName === name){
-         cart.splice(i,1)
+           cart.splice(i,1)
+           return cart;
       }else{
         return "That item is not in your cart."
       }
