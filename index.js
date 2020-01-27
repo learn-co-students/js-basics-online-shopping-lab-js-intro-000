3b631f97c5c065c2a0b0692d57de8ca34f1a3938
Blendi Ibraj <blendi.ibraj96@gmail.com>
2020-01-27 23:56:23 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemObj = {
+   itemName: `${item}`,
+   itemPrice: Math.floor(Math.random() * 100)
+ }
+ cart.push(itemObj);
+ return itemObj.itemName + " has been added to your cart."
 }
 
 function viewCart() {
