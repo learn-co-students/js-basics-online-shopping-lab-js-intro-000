c6c6f129e70810504451b3dbe4d3781c0b3b20ff
James Stamps Jr <jstampshere@gmail.com>
2020-02-15 14:43:09 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,76 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+   var itemObject = {
+      itemName:`${item}`,
+      itemPrice: Math.floor(Math.random()*100)
+    }
+      cart.push(itemObject)
+        return itemObject["itemName"] + " has been added to your cart."
 }
+
 
 function viewCart() {
-  // write your code here
+  if (cart.length===0){
+      return "Your shopping cart is empty."
+      
+  } else if (cart.length === 1) {
+      var itemObject = cart[0]
+      return `In your cart, you have ${itemObject.itemName} at $${itemObject.itemPrice}.`
+        
+  } else if (cart.length === 2) {
+      var string = "In your cart, you have " 
+        for (var i = 0; i < cart.length; i++){
+          if (i === 0){
+             string += `${cart[i].itemName} at $${cart[i].itemPrice}, and `
+           } else {
+                string += `${cart[i].itemName} at $${cart[i].itemPrice}.`
+           }
+        }                  
+        return string
+  }
+        
+   else if (cart.length >= 3) {
+        string = "In your cart, you have "
+         for (i = 0; i < cart.length; i++){
+            if (i < 2){
+              string+= `${cart[i].itemName} at $${cart[i].itemPrice}, ` 
+            }   else {
+               string+= `and ${cart[i].itemName} at $${cart[i].itemPrice}.`                  
+            } 
+         }
+   return string
+   }
+   
 }
+
 
 function total() {
-  // write your code here
+  let totalCost = 0;
+  for (let i = 0; i < cart.length; i++){
+    totalCost += cart[i].itemPrice;
+  }
+  return totalCost;
 }
+
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++){
+    if (cart[i].itemName === item){
+      cart.splice(i,1);
+      return cart;
+    }
+  }
+  return "That item is not in your cart.";
 }
 
+
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber){
+    let totalCost = total();
+    cart =[];
+    return`Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
+  } else {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
 }
