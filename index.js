5ab1b2641c1f5cc51d780d7690c479e5e5733493
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-23 22:50:11 +0000

    Done.

--- index.js
+++ index.js
@@ -20,6 +20,25 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  var statement="In your cart, you have "
+  if (cart.length > 0) {
+  for (let i=0;i<cart.length;i++) {
+    if (i != cart.length-1) {
+      statement=statement+cart[i]["itemName"]+ " at $"+cart[i]["itemPrice"]+", "
+  }
+    else if (i > 0){
+      statement=statement+"and "+cart[i]["itemName"]+ " at $"+cart[i]["itemPrice"]+"."
+    }
+    else {
+      statement=statement+cart[i]["itemName"]+ " at $"+cart[i]["itemPrice"]+"."
+
+    }
+  }
+  return statement
+}
+  else {
+    return "Your shopping cart is empty."
+  }
 }
 
 function total() {
