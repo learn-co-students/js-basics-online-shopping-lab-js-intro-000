a5c5661d1e3e3e8e03333dcb8d6192db82559bf8
Jasmine Lok <jasminellok@gmail.com>
2020-04-13 05:45:44 +0000

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem= {itemName: item, itemPrice: Math.random()*10}
+  cart.push(newItem)
+  return `${item} has been added to your cart.`
 }
 
+
 function viewCart() {
-  // write your code here
+  var inCart = ''
+  if (cart.length===0) {
+    return "Your shopping cart is empty."
+
+  } else if (cart.length===1){
+    var oneItem= `${cart[0].itemName} at $${cart[0].itemPrice}`
+    return 'In your cart, you have ' + oneItem +'.'
+
+  } else {
+    for (let i=0; i<(cart.length-1); i+=1) {
+      inCart += `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    }
+    var lastItem= `and ${cart[-1].itemName} at $${cart[-1].itemPrice}`
+    var showCart= 'In your cart, you have ' + inCart + lastItem + '.'
+
+  }
+  return showCart
 }
+
 
 function total() {
   // write your code here
