1e275b2e68cfb1888b90f9ec5e74e834e14a2585
Jasmine Lok <jasminellok@gmail.com>
2020-04-13 06:40:52 +0000

    Done.

--- index.js
+++ index.js
@@ -29,22 +29,38 @@ function viewCart() {
     for (let i=0; i<(cart.length-1); i+=1) {
       inCart += `${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
-    var lastItem= `and ${cart[-1].itemName} at $${cart[-1].itemPrice}`
+    var lastItem= `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}`
     var showCart= 'In your cart, you have ' + inCart + lastItem + '.'
-
   }
   return showCart
 }
 
 
 function total() {
-  // write your code here
+  var sum = 0
+   for (let i=0; i<(cart.length); i+=1) {
+     sum += cart[i].itemPrice
+   }
+   return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i=0; i<(cart.length); i+=1) {
+    if (cart[i].itemName === item) {
+      cart.splice(i,1)
+      return cart
+    } else {
+      return 'That item is not in your cart.'
+    }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  var cardTotal = total()
+  if (cardNumber >0) {
+    var cart = []
+    return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
+  } else {
+    return "Sorry, we don't have a credit card on file for you."
+  }
+
 }
