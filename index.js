3faef4b21471f96ff51e4a54ad34b4f2ce802036
Corey Lynch <bearinawolfpack@gmail.com>
2020-05-06 17:16:30 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function setCart(c) {
   return cart;
 }
 
-//************************* Remove Test Variables before Production************
+//************************* Disable Test Variables before Production************
 
 // var cart = [];
 
@@ -25,30 +25,7 @@ function addToCart(item) {
   return cart , `${item} has been added to your cart.`;
 }
 
-//************************* Remove Test Variables before Production************
-var myCart = [
-{  itemName:"bananas", itemPrice: 17 },
-{ itemName:"pancake batter",itemPrice: 5 },
-{ itemName:"eggs", itemPrice: 49 }
-];
-
-function viewCart() {
-  var currentCart = [];
-
-  function cond() {
-    if (num === 0) {
-      currentCart.push((`In your cart, you have ${myCart[num].itemName} at $${myCart[num].itemPrice}`));
-    } else if (num > 0 && num < myCart.length - 1) {
-        currentCart.push((`you have ${myCart[num].itemName} at $${myCart[num].itemPrice}`));
-    } else {
-        currentCart.push((`and ${myCart[num].itemName} at $${myCart[num].itemPrice}`));
-    }
-  }
-  for (num = 0; num < myCart.length; num++){
-     cond();
-  }
-  return(currentCart);
-}
+//************************* Disable Test Variables before Production************
 
 // * 1 item — `In your cart, you have bananas at $17.`
 // * 2 items — `In your cart, you have bananas at $17, and pancake batter at $5.`
@@ -58,7 +35,71 @@ function viewCart() {
 // (currentCart.push(`and ${myCart[3].itemName} at ${myCart[3].itemPrice}.`));
 // (currentCart.push(`you have ${myCart[2].itemName} at $${myCart[3].itemPrice},`));
 
-//************************* Remove Test Variables before Production************
+// function viewCart() {
+//   var currentCart = [];
+//
+//   if (cart.length === 0) {
+//     return `Your shopping cart is empty.`;
+//   } else {
+//
+//   for (n = 0; n < cart.length; n++){
+//      cond();
+//   }
+  //loop var [n] / n will work with any object size.
+  //the test wants to see an index number instead of loop variable.
+  //undefined it unrelated to hoisting issues.
+//   function cond() {
+//     if (n === 0) {
+//       currentCart.push((`In your cart, you have ${cart[n].itemName} at $${cart[n].itemPrice}`));
+//
+//     } else if (n > 0 && n < cart.length - 1) {
+//         currentCart.push((` you have ${cart[n].itemName} at $${cart[n].itemPrice}`));
+//
+//     } else {
+//         currentCart.push((` and ${cart[n].itemName} at $${cart[n].itemPrice}`));
+//     }
+//   }
+//
+//   return currentCart.toString() + '.';
+//   }
+// }
+
+// var cart = [
+// { itemName:"bananas", itemPrice: 17 },
+// { itemName:"pancake batter",itemPrice: 5 },
+// { itemName:"eggs", itemPrice: 49 }
+// ];
+
+function viewCart() {
+  var currentCart = [];
+
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  } else {
+
+  // n / num / i is undefined error is coming from loop.
+  // switched from 'for' to 'while'.
+  while (currentCart.length < cart.length){
+     cond();
+  }
+
+  function cond() {
+    if (currentCart.length === 0) {
+      currentCart.push((`In your cart, you have ${cart[currentCart.length].itemName} at $${cart[currentCart.length].itemPrice}`));
+
+    } else if (currentCart.length > 0 && currentCart.length < cart.length - 1) {
+        currentCart.push((` ${cart[currentCart.length].itemName} at $${cart[currentCart.length].itemPrice}`));
+
+    } else {
+        currentCart.push((` and ${cart[currentCart.length].itemName} at $${cart[currentCart.length].itemPrice}`));
+    }
+  }
+
+  return currentCart.toString() + '.';
+  }
+}
+
+//************************* Disable Test Variables before Production************
 
 
 
