d22614887f63caf16fd90d19fe2c8940d1d00955
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-20 23:42:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -9,22 +9,76 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(itemName) {
+      var price=1+Math.floor(Math.random()*100)
+      cart.push({"itemName":`${itemName}`, "itemPrice":price})
+      return`${itemName} has been added to your cart.`
  // write your code here
 }
 
 function viewCart() {
-  // write your code here
+// if empty
+  if (cart.length===0){
+    return "Your shopping cart is empty."
+  }
+  //if one item
+    if(cart.length===1){
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  // if two item
+  if(cart.length===2){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+}
+  //if more than 2
+  if (cart.length>2){
+    var list =`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
+    for(var i=1; i<cart.length-1; i++){
+    list += `, ${cart[i].itemName} at $${cart[i].itemPrice}`
+  }
+    list += `, and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
+    return list
+  }
 }
 
 function total() {
-  // write your code here
+var sum = 0
+  for(var i=0; i<cart.length; i++){
+sum = sum + cart[i].itemPrice
+  }
+return sum
 }
+
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i=0; i<getCart().length; i++){
+    if (getCart()[i].itemName === item){
+      getCart().splice(i,1);
+      break;}
+      }
+      return 'That item is not in your cart.'
 }
 
+
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber == null||""){
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  } else {
+    var charge = total();
+    cart=[]
+    return `Your total cost is $${charge}, which will be charged to the card ${cardNumber}.`;
+        //for(var i=0; i<getCart().length; i++){
+        }
+}
+
+
+function generateItem(itemName){
+  return{
+    itemName:itemName,
+    itemPrice:1+Math.floor(Math.random()*100)
+
+}
+}
+
+function generatePrice(){
+  var price=Math.floor(Math.random()*100)
 }
