64511975f5d5aca8e7039f5c726e1680c9e3fa24
Mike Pokalsky <mpoksky1989@gmail.com>
2020-07-15 16:20:27 +0000

    Done.

--- index.js
+++ index.js
@@ -28,12 +28,16 @@ function addToCart(item) {
 function viewCart() {
   if (cart.length === 0) {
     return `Your shopping cart is empty.`
-  } else {
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+
+  else {
     var cartItems = []
 
-    var last = `${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
+    var last = `${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
 
-    for(var i = 0; i < cart.length-1; i++){
+    for(var i = 0; i < cart.length - 1; i++){
        cartItems.push(
          `${cart[i].itemName} at $${cart[i].itemPrice}`
        )
@@ -41,7 +45,6 @@ function viewCart() {
     return `In your cart, you have ${cartItems.join(', ')}, and ${last}`
   }
 }
-
 
 function total(){
   let numbers = []
