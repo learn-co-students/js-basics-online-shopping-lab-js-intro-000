cc40c89417990bf19c26f8235695efacd7a785c8
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-27 22:24:44 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,30 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+    var newitem = { itemName : `${item}`, itemPrice : Math.floor(Math.random()*100) }
+    cart[cart.length] = newitem
+   return (`${item} has been added to your cart.`);
+
+  }
+
 
 function viewCart() {
-  // write your code here
+var tempcart = []
+var answer = []
+var l = cart.length
+  if (cart.length === 0){
+    return ("Your shopping cart is empty.")
+  }
+  else {for (let i = 0, l = cart.length-1; i < l; i++){
+      tempcart.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+  }
+      //answer = `In your cart, you have ${tempcart.join(', ')}`
+      tempcart.push(`and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`)
+      return (`In your cart, you have ${tempcart.join(', ')}`)
+
+
+    //return(`${answer}`+`, and ${cart[cart.length].itemName} at $${cart[cart.length].itemPrice}.`)
+  }
 }
 
 function total() {
