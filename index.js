f81d197ae96a5b67ebe036becb9c811ed6fe770f
Rija <73913644+Hope-24@users.noreply.github.com>
2021-01-09 22:04:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,61 @@
-var cart = [];
+cart = [];
 
-function getCart() {
- return cart;
-}
-
-function setCart(c) {
-  cart = c;
-  return cart;
+function addToCart (item) {
+    var newObj = {
+        itemName: item,
+        itemPrice: Math.floor(Math.random()*100))
+    }
+    cart.push(newObj)
+    return (`${item} has been added to your cart`)
 }
 
 function addToCart(item) {
-  var newObj = {
-    itemName: item,
-    itemPrice: Math.floor(Math.random()*100)
-  }
-  cart.push(newObj)
-  return (`${item} has been added to your cart.`)
+    var newObj = {
+        itemName: item,
+        itemPrice: Math.floor(Math.random()*100))
+    }
+    cart.push(newObj)
+    return (`${item} has been added to your cart.`)
 }
 
 function viewCart() {
-  if (cart.length === 0) {
-    return "Your shopping cart is empty."
-  } else if (cart.length === 1) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
-  } else if (cart.length === 2) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-  } else {
-    var myString = "In your cart, you have "
-    for(var i = 0; i < cart.length - 1; i++) {
-      myString = myString + `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    if (cart.length === 0) {
+        return "Your shopping cart is empty."
+    } else if (cart.length === 1) {
+        return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`
+    } else if (cart.length === 2) {
+        return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}, and ${cart[1].itemName} at ${cart[1].itemPrice}.`
+    } else 
+    myString = "In your cart, you have "
+    for (let i = 0; i < cart.length - 1; i++) {
+        myString = myString + `${cart[i].itemName} at ${cart[i].itemPrice}, `
     }
-    return myString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
-  }
+    return myString + `${cart[cart.length-1].itemName} at ${cart[cart.length-1].itemPrice}.`
 }
 
 function total() {
-  var totalCost = 0
-  for (let i = 0; i < cart.length; i++) {
-    totalCost = totalCost + cart[i].itemPrice
-    /* Also you can write totalCost =
-    totalCost + cart[i].itemPrice like totalCost+= cart[i].itemPrice, its
-    shorter notation for the same thing.*/
-  }
-  return totalCost
+    var totalCost = 0
+    for (let i = 0; i < cart.length; i++) {
+        totalCost = totalCost + cart[i].itemPrice
+    }
+    return totalCost
 }
 
 function removeFromCart(item) {
-  for (let i = 0; i < cart.length; i++) {
-    if (cart[i].itemName === item) {
-      cart.splice(i, 1)
-      return cart
+    for (let i = 0; i < cart.length; i++) {
+        if (item[i].itemName === item) {
+            cart.splice(i, 1)
+            return cart
+        }
     }
-  }
-  return "That item is not in your cart."
+    return "That item is not in your cart."
 }
 
-function placeOrder(cardNumber) {
-if (cardNumber === undefined) {
-  return "Sorry, we don't have a credit card on file for you."
-} else {
-  var totals = total()
-  cart = [];
-}
-return `Your total cost is $${totals}, which will be charged to the card ${cardNumber}.`
-}
-
-// helper functions
-function getRandomInt(min, max) {
-}
-
-function generateCartItem(itemName) {
-
-}
+function placeOrder(cardNumber) [
+    if (cardNumber === undefined) {
+        return "Sorry, we don't have a credit card on file for you."
+    } else {
+        var totals = total()
+        cart = []
+        return `Your total cost is ${totals}, which will be charged to the card ${cardNumber}.`
+    }
