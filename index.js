4327d15918c5d9c90ad04b9e2a92452c84481d6a
Chris <chris.mahfouz@hotmail.com>
2021-02-13 11:23:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-var cart = [];
+var cart=[]
+function addToCart(name){
+  var item={
+    itemName: name,
+    itemPrice:Math.floor(Math.random() * Math.floor(100))
+  }
+  cart.push(item)
+  return `${item.itemName} has been added to your cart.`
+  }
 
-function getCart() {
- return cart;
+
+function viewCart(){
+var cartString='In your cart, you have'
+if (cart.length===1){
+  cartString=`${cartString} ${cart[0].itemName} at $${cart[0].itemPrice}.`
+}
+else{
+for(let i=0;i<cart.length;i++){
+    if(cart.length-1===i){
+    cartString=`${cartString} and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+  } else {
+    cartString=`${cartString} ${cart[i].itemName} at $${cart[i].itemPrice},`
+  }
+}}
+if (cart.length===0){
+  return 'Your shopping cart is empty.'
+}
+return cartString
+  }
+
+
+function total(){
+
+}
+function removeFromCart(){
+
+}
+function placeOrder(){
+
 }
 
 function setCart(c) {
@@ -9,22 +44,6 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
-}
-
-function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
+function getCart() {
+ return cart;
 }
