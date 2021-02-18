1aac2437632447bfa3d5a2a29ddd6bab06332e11
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2021-02-18 15:29:48 -0500

    Done.

--- index.js
+++ index.js
@@ -51,17 +51,21 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
  for(let i = 0; i < cart.length; i++) {
  	if (cart[i].itemName === item) {
  		cart.splice(i, 1)
- 	} 
- 	return cart 
- } else {
-  return "That item is not in your cart."
- };
+ 		return cart 
+ 	} else {
+      return "That item is not in your cart."
+    }
+  } 
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+ if (cardNumber != 0) {
+    cart.splice(0, cart.length)
+    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+  } else {
+  return "Sorry, we don't have a credit card on file for you."
+ }
 }
