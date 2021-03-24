8696cfa0c34514111c5da8fdffe40d500a2f7bd5
ji sun lee <ellijs@hotmail.com>
2021-03-24 05:49:09 +0000

    Done.

--- index.js
+++ index.js
@@ -8,13 +8,32 @@ function setCart(c) {
   cart = c;
   return cart;
 }
-
+/*
 function addToCart(item) {
- // write your code here
+  function getPrice(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max + min) - min);
+  }
+  let newCart = {
+    itemName : "name",
+    itemPrice : getPrice(1, 100)
+  }
+  item = newCart;
+  cart.push(item);
+  return `${itemName} has been added to your cart.`
 }
-
+*/
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else {
+      let string = "In your cart, you have "
+      for (let i = 0; i < cart.length; i++) {
+      string += `${cart[itemName]} at \$ ${cart[itemPrice]}${(i === cart.length -1 ? "." : ",")}`
+      }
+      return string;
+    }
 }
 
 function total() {
